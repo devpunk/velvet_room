a0efd445c9f2b25c6d973d883dd14533632b4221
vauxhall <zero.griffin@gmail.com>
2017-08-31 14:18:34 -0500

    Add asset menu connect

--- Source/Model/Menu/MMenuItemConnect.swift
+++ Source/Model/Menu/MMenuItemConnect.swift
@@ -3,6 +3,6 @@ import UIKit
 struct MMenuItemConnect:MMenuItemProtocol
 {
     let order:MMenu.Order = MMenu.Order.connect
-    let icon:UIImage = UIImage()
+    let icon:UIImage = #imageLiteral(resourceName: "assetGenericMenuConnect")
     let controllerType:UIViewController.Type = CConnect.self
 }
