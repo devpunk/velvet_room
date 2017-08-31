173f1760583246cbd6d013099a27e8446fbd72b6
vauxhall <zero.griffin@gmail.com>
2017-08-31 14:13:16 -0500

    Add button to connect

--- Source/View/Connect/VConnect.swift
+++ Source/View/Connect/VConnect.swift
@@ -2,5 +2,35 @@ import UIKit
 
 class VConnect:ViewMain
 {
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Connect", for:UIControlState.normal)
+        button.addTarget(
+            self,
+            action:#selector(selectorActionButton(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        addSubview(button)
+        
+        NSLayoutConstraint.equals(
+            view:button,
+            toView:self)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: selectors
+    
+    @objc
+    private func selectorActionButton(sender button:UIButton)
+    {
+        
+    }
 }
