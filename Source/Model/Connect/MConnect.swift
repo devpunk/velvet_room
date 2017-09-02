1c4553c4109e8d01759b3cc72602b19063413a83
vauxhall <zero.griffin@gmail.com>
2017-09-02 04:44:07 -0500

    Add input stream

--- Source/Model/Connect/MConnect.swift
+++ Source/Model/Connect/MConnect.swift
@@ -2,10 +2,37 @@ import UIKit
 
 class MConnect:Model<ArchConnect>
 {
-    let requestPort:UInt16 = 9309
+    var delegate:ConnectDelegate?
+    var inputStream:InputStream?
+    var outputStream:OutputStream?
+    let requestPort:Int = 9309
 
     func startWireless()
     {
+        delegate = ConnectDelegate()
+
+        Stream.getStreamsToHost(
+            withName:"1.1.1.1",
+            port:8080,
+            inputStream: &inputStream,
+            outputStream: nil)
         
+        outputStream?.delegate = delegate
+        inputStream?.delegate = delegate
+
+        inputStream?.schedule(in: RunLoop.main, forMode: RunLoopMode.commonModes)
+        outputStream?.schedule(in: RunLoop.current, forMode: RunLoopMode.commonModes)
+        inputStream?.open()
+        outputStream?.open()
+    }
+}
+
+class ConnectDelegate:NSObject, StreamDelegate
+{
+    
+    func stream(_ aStream: Stream, handle eventCode: Stream.Event)
+    {
+        print(eventCode)
+        print(aStream.streamError?.localizedDescription)
     }
 }
