50aa5deb153d2be0f9bcc3948ca07919e6455bbf
vauxhall <zero.griffin@gmail.com>
2017-09-03 20:44:41 -0500

    Create external delegate

--- Source/Model/Connect/MConnect.swift
+++ Source/Model/Connect/MConnect.swift
@@ -1,16 +1,26 @@
 import UIKit
 import CocoaAsyncSocket
 
-class MConnect:Model<ArchConnect>, GCDAsyncUdpSocketDelegate
+class MConnect:Model<ArchConnect>
 {
     let requestPort:Int = 9309
+    var delegate:MConnectDelegate?
     var socket:GCDAsyncUdpSocket?
 
     func startWireless()
     {
+        delegate = MConnectDelegate()
+        
         socket = GCDAsyncUdpSocket(
-            delegate:self,
+            delegate:delegate,
             delegateQueue:DispatchQueue.global(qos:DispatchQoS.QoSClass.background),
             socketQueue:DispatchQueue.global(qos:DispatchQoS.QoSClass.background))
     }
+    
+    
+}
+
+class MConnectDelegate:NSObject, GCDAsyncUdpSocketDelegate
+{
+    
 }
