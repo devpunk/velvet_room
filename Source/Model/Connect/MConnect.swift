db434bf90528674e65308d6aa04a697cc4e9fe7e
vauxhall <zero.griffin@gmail.com>
2017-09-03 20:43:39 -0500

    Create delegate

--- Source/Model/Connect/MConnect.swift
+++ Source/Model/Connect/MConnect.swift
@@ -1,10 +1,16 @@
 import UIKit
+import CocoaAsyncSocket
 
-class MConnect:Model<ArchConnect>
+class MConnect:Model<ArchConnect>, GCDAsyncUdpSocketDelegate
 {
     let requestPort:Int = 9309
+    var socket:GCDAsyncUdpSocket?
 
     func startWireless()
     {
+        socket = GCDAsyncUdpSocket(
+            delegate:self,
+            delegateQueue:DispatchQueue.global(qos:DispatchQoS.QoSClass.background),
+            socketQueue:DispatchQueue.global(qos:DispatchQoS.QoSClass.background))
     }
 }
