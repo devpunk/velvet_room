bd5b43a96ca81470d73888c1e320e6dab219da53
vauxhall <zero.griffin@gmail.com>
2017-09-08 19:57:01 -0500

    Read xml in ascii

--- Source/Model/Connect/PTPDelegate.swift
+++ Source/Model/Connect/PTPDelegate.swift
@@ -175,19 +175,25 @@ class PTPDelegate:NSObject, GCDAsyncSocketDelegate
                 
                 print("total data: \(datareceived.count)")
                 
-                self.dataReceived = nil
+                
                 if let receivingString:String = String(
                     data:datareceived,
-                    encoding:String.Encoding.utf8)
+                    encoding:String.Encoding.ascii)
                 {
                     print("data in xml:")
                     print(receivingString)
+                    
+                    /**
+ 
+                     <VITAInformation responderVersion="3.65" protocolVersion="01800010" comVersion="190" modelInfo="PCH02006ZA11" timezone="10"><photoThumb type="0" codecType="17" width="213" height="120"/><videoThumb type="1" codecType="5" width="213" height="120" duration="15"/><musicThumb type="0" codecType="17" width="192" height="192"/><gameThumb type="0" codecType="17" width="192" height="192"/></VITAInformation>
+ 
+ ***/
                 }
                 else
                 {
                     print("can't create string")
                 }
-                
+                self.dataReceived = nil
             }
             else if header.type == 10
             {
