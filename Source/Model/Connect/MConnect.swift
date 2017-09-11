25487a80eeb1a5b5d461fd202e276dfcc59d99e7
vauxhall <zero.griffin@gmail.com>
2017-09-11 16:37:17 -0500

    Factory walk items

--- Source/Model/Connect/MConnect.swift
+++ Source/Model/Connect/MConnect.swift
@@ -2,10 +2,13 @@ import UIKit
 
 final class MConnect:Model<ArchConnect>
 {
+    let itemsWalk:[MConnectWalkProtocol]
     private(set) var status:MConnectStatusProtocol?
 
     required init()
     {
+        itemsWalk = MConnect.factoryWalk()
+        
         super.init()
         statusStandby()
     }
--- Source/Model/Connect/MConnectFactory.swift
+++ Source/Model/Connect/MConnectFactory.swift
@@ -2,5 +2,15 @@ import Foundation
 
 extension MConnect
 {
+    //MARK: internal
     
+    class func factoryWalk() -> [MConnectWalkProtocol]
+    {
+        let itemCover:MConnectWalkCover = MConnectWalkCover()
+        
+        let items:[MConnectWalkProtocol] = [
+            itemCover]
+        
+        return items
+    }
 }
