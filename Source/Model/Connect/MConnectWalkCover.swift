b8daf5cbd9cf516bf926a8ad8cc5937d461d0484
vauxhall <zero.griffin@gmail.com>
2017-09-11 16:48:53 -0500

    Add cover properties

--- Source/Model/Connect/MConnectWalkCover.swift
+++ Source/Model/Connect/MConnectWalkCover.swift
@@ -1,6 +1,10 @@
-import Foundation
+import UIKit
 
 struct MConnectWalkCover:MConnectWalkProtocol
 {
-    
+    let icon:UIImage = #imageLiteral(resourceName: "assetConnectWalkIcon")
+    let title:String = String.localizedModel(
+        key:"MConnectWalkCover_title")
+    let descr:String = String.localizedModel(
+        key:"MConnectWalkCover_descr")
 }
