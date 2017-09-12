254443c7e6fcfdab742e46b53a1cc1d33798423f
vauxhall <zero.griffin@gmail.com>
2017-09-12 18:21:22 -0500

    Add view spinner

--- Source/View/Connect/VConnectLoading.swift
+++ Source/View/Connect/VConnectLoading.swift
@@ -2,5 +2,30 @@ import UIKit
 
 final class VConnectLoading:View<ArchConnect>
 {
+    private weak var spinner:VSpinner!
     
+    required init(controller:CConnect)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        
+        let spinner:VSpinner = VSpinner()
+        self.spinner = spinner
+        
+        addSubview(spinner)
+        
+        NSLayoutConstraint.equals(
+            view:spinner,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    deinit
+    {
+        spinner.stopAnimating()
+    }
 }
