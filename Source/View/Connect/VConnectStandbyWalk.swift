956a69f707e9822b2366b5f2ab68c3614422cf8a
vauxhall <zero.griffin@gmail.com>
2017-09-12 09:13:11 -0500

    Add number of items at section

--- Source/View/Connect/VConnectStandbyWalk.swift
+++ Source/View/Connect/VConnectStandbyWalk.swift
@@ -46,6 +46,21 @@ final class VConnectStandbyWalk:VCollection<
         return cellSize
     }
     
+    override func numberOfSections(
+        in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.itemsWalk.count
+        
+        return count
+    }
+    
     override func collectionView(
         _ collectionView:UICollectionView,
         cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
