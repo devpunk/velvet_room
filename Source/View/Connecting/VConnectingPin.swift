b8abb673db83e95737b3965842a9e5e2b22626b9
vauxhall <zero.griffin@gmail.com>
2017-09-12 20:55:02 -0500

    Add title to view pin

--- Source/View/Connecting/VConnectingPin.swift
+++ Source/View/Connecting/VConnectingPin.swift
@@ -2,5 +2,43 @@ import UIKit
 
 class VConnectingPin:View<ArchConnecting>
 {
+    private let kTitleBottom:CGFloat = -150
+    private let kTitleHeight:CGFloat = 60
     
+    required init(controller:CConnecting)
+    {
+        super.init(controller:controller)
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.font = UIFont.medium(size:15)
+        labelTitle.textColor = UIColor.white
+        labelTitle.textAlignment = NSTextAlignment.center
+        labelTitle.text = String.localizedView(
+            key:"VConnectingPin_labelTitle")
+        
+        addSubview(labelTitle)
+        
+        NSLayoutConstraint.equals(
+            view:spinner,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:labelTitle,
+            toView:self,
+            constant:kTitleBottom)
+        NSLayoutConstraint.height(
+            view:labelTitle,
+            constant:kTitleHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:labelTitle,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
