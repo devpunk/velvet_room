6c8eb08b1183d7e62e99f05044b392659969ebab
vauxhall <zero.griffin@gmail.com>
2017-09-12 19:36:35 -0500

    Use renamed views

--- Source/View/Connect/VConnect.swift
+++ Source/View/Connect/VConnect.swift
@@ -2,14 +2,56 @@ import UIKit
 
 final class VConnect:ViewMain
 {
+    private let kStartHeight:CGFloat = 64
+    private let kStartBottom:CGFloat = -90
+    
     required init(controller:UIViewController)
     {
         super.init(controller:controller)
         
+        guard
+        
+            let controller:CConnect = controller as? CConnect
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: internal
+    
+    func factoryViews(controller:CConnect)
+    {
+        let viewWalk:VConnectStandbyWalk = VConnectStandbyWalk(
+            controller:controller)
+        
+        let viewStart:VConnectStandbyStart = VConnectStandbyStart(
+            controller:controller)
+        
+        addSubview(viewWalk)
+        addSubview(viewStart)
+        
+        NSLayoutConstraint.equals(
+            view:viewWalk,
+            toView:self)
+        
+        NSLayoutConstraint.height(
+            view:viewStart,
+            constant:kStartHeight)
+        NSLayoutConstraint.bottomToBottom(
+            view:viewStart,
+            toView:self,
+            constant:kStartBottom)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewStart,
+            toView:self)
     }
 }
