6573e92423d7f18bc70a3ad650014d3e4c97484f
vauxhall <zero.griffin@gmail.com>
2017-09-13 04:02:55 -0500

    Disable interaction in collection

--- Source/View/Connecting/VConnectingPinList.swift
+++ Source/View/Connecting/VConnectingPinList.swift
@@ -4,10 +4,23 @@ final class VConnectingPinList:VCollection<
     ArchConnecting,
     VConnectingPinListCell>
 {
+    private let kInterItem:CGFloat = 5
+    
     required init(controller:CConnecting)
     {
         super.init(controller:controller)
+        isUserInteractionEnabled = false
         
+        collectionView.isUserInteractionEnabled = false
+        collectionView.bounces = false
+        collectionView.isScrollEnabled = false
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+            flow.minimumInteritemSpacing = kInterItem
+            flow.minimumLineSpacing = kInterItem
+        }
     }
     
     required init?(coder:NSCoder)
