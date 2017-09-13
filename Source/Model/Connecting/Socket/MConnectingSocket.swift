158027831e768bf22ac89d6e5ac7793c77ea36b0
vauxhall <zero.griffin@gmail.com>
2017-09-13 12:03:42 -0500

    Create view connecting error

--- Source/Model/Connecting/Socket/MConnectingSocket.swift
+++ Source/Model/Connecting/Socket/MConnectingSocket.swift
@@ -2,11 +2,24 @@ import Foundation
 
 final class MConnectingSocket
 {
+    private var configuration:MConnectingSocketConfiguration?
+    
+    //MARK: private
+    
+    private func asyncStart()
+    {
+        
+    }
+    
     //MARK: internal
     
     func start()
     {
-        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.asyncStart()
+        }
     }
     
     func cancel()
--- Source/Model/Connecting/Socket/MConnectingSocketConfiguration.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketConfiguration.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class MConnectingSocketConfiguration
+final class MConnectingSocketConfiguration
 {
     
 }
