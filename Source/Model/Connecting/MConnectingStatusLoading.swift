9252ad08d89c74746933a3b278d2c706f3a0df16
vauxhall <zero.griffin@gmail.com>
2017-09-13 10:13:54 -0500

    Add button title to models

--- Source/Model/Connecting/MConnectingStatusLoading.swift
+++ Source/Model/Connecting/MConnectingStatusLoading.swift
@@ -3,4 +3,6 @@ import Foundation
 struct MConnectingStatusLoading:MConnectingStatusProtocol
 {
     let viewType:View<ArchConnecting>.Type = VConnectingLoading.self
+    let buttonTitle:String = String.localizedModel(
+        key:"MConnectingStatusLoading_buttonTitle")
 }
--- Source/Model/Connecting/MConnectingStatusPin.swift
+++ Source/Model/Connecting/MConnectingStatusPin.swift
@@ -3,4 +3,6 @@ import Foundation
 struct MConnectingStatusPin:MConnectingStatusProtocol
 {
     let viewType:View<ArchConnecting>.Type = VConnectingPin.self
+    let buttonTitle:String = String.localizedModel(
+        key:"MConnectingStatusPin_buttonTitle")
 }
