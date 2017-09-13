cc3c8d723fb75fb015ee34e6ce726ec12de69680
vauxhall <zero.griffin@gmail.com>
2017-09-12 19:28:04 -0500

    Use status bar light

--- Source/Controller/Connecting/CConnecting.swift
+++ Source/Controller/Connecting/CConnecting.swift
@@ -1,6 +1,12 @@
-import Foundation
+import UIKit
 
 class CConnecting:Controller<ArchConnecting>
 {
-    
+    override var preferredStatusBarStyle:UIStatusBarStyle
+    {
+        get
+        {
+            return UIStatusBarStyle.lightContent
+        }
+    }
 }
