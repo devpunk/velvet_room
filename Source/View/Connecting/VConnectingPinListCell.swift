6bf80a675bdce13f44a8d4bb56b12af065ec14d2
vauxhall <zero.griffin@gmail.com>
2017-09-13 03:34:42 -0500

    Add background view to cell

--- Source/View/Connecting/VConnectingPinListCell.swift
+++ Source/View/Connecting/VConnectingPinListCell.swift
@@ -2,9 +2,24 @@ import UIKit
 
 class VConnectingPinListCell:UICollectionViewCell
 {
+    private let kCornerRadius:CGFloat = 6
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
+        isUserInteractionEnabled = false
+        
+        let background:UIView = UIView()
+        background.isUserInteractionEnabled = false
+        background.backgroundColor = UIColor(white:1, alpha:0.6)
+        background.translatesAutoresizingMaskIntoConstraints = false
+        background.layer.cornerRadius = kCornerRadius
+        
+        addSubview(background)
+        
+        NSLayoutConstraint.equals(
+            view:background,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
