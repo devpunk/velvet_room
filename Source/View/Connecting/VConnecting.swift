76ce47118110fe399171ad423abb2cd155b9d5de
vauxhall <zero.griffin@gmail.com>
2017-09-12 20:04:59 -0500

    Move views to loading

--- Source/View/Connecting/VConnecting.swift
+++ Source/View/Connecting/VConnecting.swift
@@ -2,12 +2,10 @@ import UIKit
 
 class VConnecting:ViewMain
 {
-    private weak var spinner:VSpinner!
     private weak var layoutCancelLeft:NSLayoutConstraint!
     private let kCancelBottom:CGFloat = -80
     private let kCancelHeight:CGFloat = 45
     private let kCancelWidth:CGFloat = 140
-    private let kTitleHeight:CGFloat = 22
     
     required init(controller:UIViewController)
     {
@@ -28,11 +26,6 @@ class VConnecting:ViewMain
     required init?(coder:NSCoder)
     {
         return nil
-    }
-    
-    deinit
-    {
-        spinner.stopAnimating()
     }
     
     override func layoutSubviews()
@@ -70,19 +63,6 @@ class VConnecting:ViewMain
             colourTop:UIColor.colourGradientLight,
             colourBottom:UIColor.colourGradientDark)
         
-        let spinner:VSpinner = VSpinner()
-        self.spinner = spinner
-        
-        let labelTitle:UILabel = UILabel()
-        labelTitle.isUserInteractionEnabled = false
-        labelTitle.translatesAutoresizingMaskIntoConstraints = false
-        labelTitle.backgroundColor = UIColor.clear
-        labelTitle.font = UIFont.medium(size:15)
-        labelTitle.textColor = UIColor.white
-        labelTitle.textAlignment = NSTextAlignment.center
-        labelTitle.text = String.localizedView(
-            key:"VConnecting_labelTitle")
-        
         let buttonCancel:UIButton = UIButton()
         buttonCancel.translatesAutoresizingMaskIntoConstraints = false
         buttonCancel.setTitleColor(
@@ -101,16 +81,10 @@ class VConnecting:ViewMain
             for:UIControlEvents.touchUpInside)
         
         addSubview(viewGradient)
-        addSubview(spinner)
         addSubview(buttonCancel)
-        addSubview(labelTitle)
         
         NSLayoutConstraint.equals(
             view:viewGradient,
-            toView:self)
-        
-        NSLayoutConstraint.equals(
-            view:spinner,
             toView:self)
         
         NSLayoutConstraint.bottomToBottom(
@@ -125,16 +99,6 @@ class VConnecting:ViewMain
             constant:kCancelWidth)
         layoutCancelLeft = NSLayoutConstraint.leftToLeft(
             view:buttonCancel,
-            toView:self)
-        
-        NSLayoutConstraint.bottomToTop(
-            view:labelTitle,
-            toView:buttonCancel)
-        NSLayoutConstraint.height(
-            view:labelTitle,
-            constant:kCancelHeight)
-        NSLayoutConstraint.equalsHorizontal(
-            view:labelTitle,
             toView:self)
     }
 }
--- Source/View/Connecting/VConnectingLoading.swift
+++ Source/View/Connecting/VConnectingLoading.swift
@@ -2,5 +2,50 @@ import UIKit
 
 class VConnectingLoading:View<ArchConnecting>
 {
+    private weak var spinner:VSpinner!
+    private let kTitleBottom:CGFloat = -135
+    private let kTitleHeight:CGFloat = 22
     
+    required init(controller:CConnecting)
+    {
+        super.init(controller:controller)
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.font = UIFont.medium(size:15)
+        labelTitle.textColor = UIColor.white
+        labelTitle.textAlignment = NSTextAlignment.center
+        labelTitle.text = String.localizedView(
+            key:"VConnectingLoading_labelTitle")
+        
+        addSubview(spinner)
+        addSubview(labelTitle)
+        
+        NSLayoutConstraint.equals(
+            view:spinner,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:labelTitle,
+            toView:self,
+            constant:kTitleBottom)
+        NSLayoutConstraint.height(
+            view:labelTitle,
+            constant:kTitleHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:labelTitle,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    deinit
+    {
+        spinner.stopAnimating()
+    }
 }
