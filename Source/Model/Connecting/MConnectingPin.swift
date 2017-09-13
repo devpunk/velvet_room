f32920d6a523d7dccf1049331bab74aed6fb03d6
vauxhall <zero.griffin@gmail.com>
2017-09-13 04:01:05 -0500

    Add pin methods

--- Source/Model/Connecting/MConnectingPin.swift
+++ Source/Model/Connecting/MConnectingPin.swift
@@ -2,5 +2,33 @@ import Foundation
 
 class MConnectingPin
 {
+    private var pinString:String
+    private(set) var length:Int
     
+    init()
+    {
+        pinString = MConnectingPin.generatePin()
+        length = pinString.count
+    }
+    
+    //MARK: internal
+    
+    func digitAtIndex(index:Int) -> String
+    {
+        let startIndex:String.Index = pinString.startIndex
+        let digitIndex:String.Index = pinString.index(
+            startIndex,
+            offsetBy:index)
+        let digitCharacter:Character = pinString[digitIndex]
+        let digitString:String = String(digitCharacter)
+        
+        return digitString
+    }
+    
+    func validatePin(pinString:String) -> Bool
+    {
+        let equal:Bool = self.pinString == pinString
+        
+        return equal
+    }
 }
--- Source/Model/Connecting/MConnectingPinFactory.swift
+++ Source/Model/Connecting/MConnectingPinFactory.swift
@@ -2,5 +2,30 @@ import Foundation
 
 extension MConnectingPin
 {
+    private static let kFallBackPin:String = "000000000"
+    private static let kPinLength:Int = 8
+    private static let kMaxPinRandom:UInt32 = 99999999
     
+    class func generatePin() -> String
+    {
+        let numberFormatter:NumberFormatter = NumberFormatter()
+        numberFormatter.maximumIntegerDigits = MConnectingPin.kPinLength
+        numberFormatter.minimumIntegerDigits = MConnectingPin.kPinLength
+        
+        let random:UInt32 = arc4random_uniform(
+            MConnectingPin.kMaxPinRandom)
+        let number:NSNumber = random as NSNumber
+        
+        guard
+        
+            let pinString:String = numberFormatter.string(
+                from:number)
+        
+        else
+        {
+            return MConnectingPin.kFallBackPin
+        }
+        
+        return pinString
+    }
 }
