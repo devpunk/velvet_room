a5a0bd2d96da2e9f8da6949726cee1efc2ee73be
vauxhall <zero.griffin@gmail.com>
2017-09-14 18:24:20 -0500

    Factory socket queues

--- Source/Model/Connecting/Socket/MConnectingSocketFactory.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketFactory.swift
@@ -1,5 +1,34 @@
 import Foundation
+import CocoaAsyncSocket
 
 extension MConnectingSocket
 {
+    private static let kUdpQueueLabel:String = "velvetRoom.connecting.udp"
+    private static let kTcpQueueLabel:String = "velvetRoom.connecting.tcp"
+    
+    //MARK: private
+    
+    private class func factoryUdpQueue() -> DispatchQueue
+    {
+        let queue = DispatchQueue(
+            label:kUdpQueueLabel,
+            qos:DispatchQoS.background,
+            attributes:DispatchQueue.Attributes(),
+            autoreleaseFrequency:DispatchQueue.AutoreleaseFrequency.inherit,
+            target:DispatchQueue.global(qos:DispatchQoS.QoSClass.background))
+        
+        return queue
+    }
+    
+    private class func factoryTcpQueue() -> DispatchQueue
+    {
+        let queue = DispatchQueue(
+            label:kTcpQueueLabel,
+            qos:DispatchQoS.background,
+            attributes:DispatchQueue.Attributes(),
+            autoreleaseFrequency:DispatchQueue.AutoreleaseFrequency.inherit,
+            target:DispatchQueue.global(qos:DispatchQoS.QoSClass.background))
+        
+        return queue
+    }
 }
