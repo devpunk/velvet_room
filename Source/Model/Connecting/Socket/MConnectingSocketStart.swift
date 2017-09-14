d4d656ae102f4378e1a87b6c05b56224d6fbd3dc
vauxhall <zero.griffin@gmail.com>
2017-09-14 18:39:56 -0500

    Move start to extension

--- Source/Model/Connecting/Socket/MConnectingSocketStart.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketStart.swift
@@ -2,5 +2,38 @@ import Foundation
 
 extension MConnectingSocket
 {
+    //MARK: private
     
+    private func asyncStart()
+    {
+        guard
+            
+            let configuration:MVitaConfiguration = MVitaConfiguration.factoryConfiguration()
+            
+        else
+        {
+            failedConfiguration()
+            return
+        }
+        
+        self.configuration = configuration
+    }
+    
+    private func failedConfiguration()
+    {
+        let message:String = String.localizedModel(
+            key:"MConnectingSocket_failedConfiguration")
+        model?.foundError(errorMessage:message)
+    }
+    
+    //MARK: internal
+    
+    func start()
+    {
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.asyncStart()
+        }
+    }
 }
