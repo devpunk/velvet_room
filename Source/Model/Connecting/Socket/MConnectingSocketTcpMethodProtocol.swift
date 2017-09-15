17238d66e8a931f6ef3c60cbe4c15febc1b4f78d
vauxhall <zero.griffin@gmail.com>
2017-09-15 18:22:05 -0500

    Add type property to protocol

--- Source/Model/Connecting/Socket/MConnectingSocketTcpMethodProtocol.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketTcpMethodProtocol.swift
@@ -2,6 +2,8 @@ import Foundation
 
 protocol MConnectingSocketTcpMethodProtocol
 {
+    var type:MConnectingSocketTcpMethodType { get }
+    
     init(received:String)
     func strategy(model:MConnectingSocketTcp)
 }
