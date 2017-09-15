91e802ea97e948587ed77dabcbffc8c69698ae76
vauxhall <zero.griffin@gmail.com>
2017-09-15 09:04:55 -0500

    Disable interaction on home
    Allow session to be loaded before
    user changes tab

--- Source/Controller/Home/CHome.swift
+++ Source/Controller/Home/CHome.swift
@@ -2,5 +2,17 @@ import Foundation
 
 final class CHome:Controller<ArchHome>
 {
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        parentController?.view.isUserInteractionEnabled = false
+    }
     
+    //MARK: internal
+    
+    func sessionLoaded()
+    {
+        parentController?.view.isUserInteractionEnabled = true
+    }
 }
