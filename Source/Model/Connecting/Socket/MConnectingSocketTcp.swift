3b84b2037061942b61e6531ae03b4e06389c570f
vauxhall <zero.griffin@gmail.com>
2017-09-14 19:06:18 -0500

    Init tcp socket

--- Source/Model/Connecting/Socket/MConnectingSocketTcp.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketTcp.swift
@@ -1,6 +1,22 @@
 import Foundation
+import CocoaAsyncSocket
 
 final class MConnectingSocketTcp
 {
+    private let socket:GCDAsyncSocket
+    private let delegate:MConnectingSocketTcpDelegate
+    private let queue:DispatchQueue
+    private let configuration:MVitaConfiguration
     
+    init(
+        socket:GCDAsyncSocket,
+        delegate:MConnectingSocketTcpDelegate,
+        queue:DispatchQueue,
+        configuration:MVitaConfiguration)
+    {
+        self.socket = socket
+        self.delegate = delegate
+        self.queue = queue
+        self.configuration = configuration
+    }
 }
