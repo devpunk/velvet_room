553941352e34c17d1903a6d9c8c45621c3e49cc7
vauxhall <zero.griffin@gmail.com>
2017-09-15 07:28:37 -0500

    Create device id

--- Source/CoreData/Object/DAccount.swift
+++ Source/CoreData/Object/DAccount.swift
@@ -2,5 +2,20 @@ import Foundation
 
 extension DAccount
 {
+    //MARK: internal
     
+    func createDeviceId()
+    {
+        guard
+            
+            deviceId == nil
+        
+        else
+        {
+            return
+        }
+        
+        let uuid:String = UUID.init().uuidString
+        deviceId = uuid
+    }
 }
