bd811d2c7ac7b17373a3372e5320704be85cfcf1
vauxhall <zero.griffin@gmail.com>
2017-09-15 18:36:15 -0500

    Add strategy methods

--- Source/Model/Connecting/Socket/MConnectingSocketTcpStrategies.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketTcpStrategies.swift
@@ -2,5 +2,20 @@ import Foundation
 
 extension MConnectingSocketTcp
 {
+    //MARK: internal
     
+    func strategyAnswerOk()
+    {
+        
+    }
+    
+    func strategyAnswerConnected()
+    {
+        
+    }
+    
+    func strategyAnswerPinError()
+    {
+        
+    }
 }
