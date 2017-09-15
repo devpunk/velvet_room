994da7c7042249d7161f05354661af2e3add849a
vauxhall <zero.griffin@gmail.com>
2017-09-15 17:23:12 -0500

    Add method type cases

--- Source/Model/Connecting/Socket/MConnectingSocketTcpMethodType.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketTcpMethodType.swift
@@ -1,6 +1,11 @@
 import Foundation
 
-enum MConnectingSocketTcpMethodType
+enum MConnectingSocketTcpMethodType:String
 {
-    
+    case showpin = "SHOWPIN"
+    case register = "REGISTER"
+    case registerResult = "REGISTERRESULT"
+    case registerCancel = "REGISTERCANCEL"
+    case connect = "CONNECT"
+    case standby = "STANDBY"
 }
