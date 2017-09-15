ed9faabe00d5e1a85beb05e36979240166f51643
vauxhall <zero.griffin@gmail.com>
2017-09-15 17:53:42 -0500

    Factory method

--- Source/Model/Connecting/Socket/MConnectingSocketTcpMethodProtocol.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketTcpMethodProtocol.swift
@@ -2,5 +2,5 @@ import Foundation
 
 protocol MConnectingSocketTcpMethodProtocol
 {
-    
+    init()
 }
--- Source/Model/Connecting/Socket/MConnectingSocketTcpMethodTypeFactory.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketTcpMethodTypeFactory.swift
@@ -2,5 +2,55 @@ import Foundation
 
 extension MConnectingSocketTcpMethodType
 {
+    private static func factoryMethod(
+        type:MConnectingSocketTcpMethodType) -> MConnectingSocketTcpMethodProtocol.Type
+    {
+        switch type
+        {
+        case MConnectingSocketTcpMethodType.showpin:
+            
+            return MConnectingSocketTcpMethodShowpin.self
+            
+        case MConnectingSocketTcpMethodType.register:
+            
+            return MConnectingSocketTcpMethodRegister.self
+            
+        case MConnectingSocketTcpMethodType.registerResult:
+            
+            return MConnectingSocketTcpMethodRegisterResult.self
+            
+        case MConnectingSocketTcpMethodType.registerCancel:
+            
+            return MConnectingSocketTcpMethodRegisterCancel.self
+            
+        case MConnectingSocketTcpMethodType.connect:
+            
+            return MConnectingSocketTcpMethodConnect.self
+            
+        case MConnectingSocketTcpMethodType.standby:
+            
+            return MConnectingSocketTcpMethodStandby.self
+        }
+    }
+    
     //MARK: internal
+    
+    static func factoryMethod(string:String) -> MConnectingSocketTcpMethodProtocol?
+    {
+        guard
+        
+            let type:MConnectingSocketTcpMethodType = MConnectingSocketTcpMethodType(
+                rawValue:string)
+        
+        else
+        {
+            return nil
+        }
+        
+        let methodType:MConnectingSocketTcpMethodProtocol.Type = factoryMethod(
+            type:type)
+        let method:MConnectingSocketTcpMethodProtocol = methodType.init()
+        
+        return method
+    }
 }
