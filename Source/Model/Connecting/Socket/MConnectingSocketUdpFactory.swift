ca7691ff91e6dee3d52f28e0387f498e5d018fd2
vauxhall <zero.griffin@gmail.com>
2017-09-15 11:14:39 -0500

    Fetch device id

--- Source/Model/Connecting/Socket/MConnectingSocketUdpFactory.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketUdpFactory.swift
@@ -2,5 +2,40 @@ import Foundation
 
 extension MConnectingSocketUdp
 {
+    //MARK: private
     
+    private func fetchDeviceId(completion:@escaping((String) -> ()))
+    {
+        guard
+        
+            let database:Database = Database(bundle:nil)
+        
+        else
+        {
+            return
+        }
+        
+        database.fetch
+        { (accountList:[DAccount]) in
+            
+            guard
+            
+                let account:DAccount = accountList.first,
+                let deviceId:String = account.deviceId
+            
+            else
+            {
+                return
+            }
+            
+            completion(deviceId)
+        }
+    }
+    
+    //MARK: internal
+    
+    func factoryReplyData() -> Data?
+    {
+        return nil
+    }
 }
