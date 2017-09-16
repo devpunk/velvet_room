4a3815eb489d6b3e97d31e003fbdbd430c4696a7
vauxhall <zero.griffin@gmail.com>
2017-09-16 00:42:22 -0500

    Create extension for tools

--- Source/Model/Vita/MVitaConfigurationFactory.swift
+++ Source/Model/Vita/MVitaConfigurationFactory.swift
@@ -4,57 +4,7 @@ extension MVitaConfiguration
 {
     //MARK: private
     
-    private static func factoryMap() -> [String:Any]?
-    {
-        guard
-        
-            let resourceUrl:URL = Bundle.main.url(
-                forResource:kResourceName,
-                withExtension:kResourceExtension)
-        
-        else
-        {
-            return nil
-        }
-        
-        let data:Data
-        
-        do
-        {
-            try data = Data(
-                contentsOf:resourceUrl,
-                options:Data.ReadingOptions.mappedIfSafe)
-        }
-        catch
-        {
-            return nil
-        }
-        
-        let propertyList:Any
-        
-        do
-        {
-            try propertyList = PropertyListSerialization.propertyList(
-                from:data,
-                options:PropertyListSerialization.ReadOptions(),
-                format:nil)
-        }
-        catch
-        {
-            return nil
-        }
-        
-        guard
-        
-            let map:[String:Any] = propertyList as? [String:Any]
-        
-        else
-        {
-            return nil
-        }
-        
-        return map
-    }
+    
     
     private static func factoryConfigurationBroadcast(
         map:[String:Any]) -> MVitaConfigurationBroadcast?
@@ -125,21 +75,6 @@ extension MVitaConfiguration
             port:port)
         
         return configuration
-    }
-    
-    private static func removeDoubleScaping(string:String) -> String
-    {
-        let cleanedNewLine:String = string.replacingOccurrences(
-            of:"\\n",
-            with:"\n")
-        let cleanedReturn:String = cleanedNewLine.replacingOccurrences(
-            of:"\\r",
-            with:"\r")
-        let cleanedEnd:String = cleanedReturn.replacingOccurrences(
-            of:"\\0",
-            with:"\0")
-        
-        return cleanedEnd
     }
     
     //MARK: internal
