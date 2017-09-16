791ec1d011f36e026c58d02ce2c13992457f62f3
vauxhall <zero.griffin@gmail.com>
2017-09-16 00:51:08 -0500

    Create broadcast extension

--- Source/Model/Vita/MVitaConfigurationFactory.swift
+++ Source/Model/Vita/MVitaConfigurationFactory.swift
@@ -4,53 +4,6 @@ extension MVitaConfiguration
 {
     //MARK: private
     
-    
-    
-    private static func factoryConfigurationBroadcast(
-        map:[String:Any]) -> MVitaConfigurationBroadcast?
-    {
-        guard
-            
-            let mapBroadcast:[String:Any] = map[
-                kKeyBroadcast] as? [String:Any],
-            let searchCommand:String = mapBroadcast[
-                kKeyBroadcastSearchCommand] as? String,
-            let searchProtocol:String = mapBroadcast[
-                kKeyBroadcastSearchProtocol] as? String,
-            let replyAvailable:String = mapBroadcast[
-                kKeyBroadcastReplyAvailable] as? String,
-            let replyOk:String = mapBroadcast[
-                kKeyBroadcastReplyOk] as? String,
-            let replyPinError:String = mapBroadcast[
-                kKeyBroadcastReplyPinError] as? String,
-            let replyConnected:String = mapBroadcast[
-                kKeyBroadcastReplyConnected] as? String,
-            let replyConnectingError:String = mapBroadcast[
-                kKeyBroadcastReplyConnectingError] as? String,
-            let pinTitle:String = mapBroadcast[
-                kKeyBroadcastPinTitle] as? String
-        
-        else
-        {
-            return nil
-        }
-        
-        let cleanedReplyAvailable:String = removeDoubleScaping(
-            string:replyAvailable)
-        
-        let broadcast:MVitaConfigurationBroadcast = MVitaConfigurationBroadcast(
-            searchCommand:searchCommand,
-            searchProtocol:searchProtocol,
-            replyAvailable:cleanedReplyAvailable,
-            replyOk:replyOk,
-            replyPinError:replyPinError,
-            replyConnected:replyConnected,
-            replyConnectingError:replyConnectingError,
-            pinTitle:pinTitle)
-        
-        return broadcast
-    }
-    
     private static func factoryConfiguration(
         map:[String:Any]) -> MVitaConfiguration?
     {
