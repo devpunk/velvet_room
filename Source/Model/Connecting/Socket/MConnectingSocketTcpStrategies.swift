a6cf1f33f54066046a09fba430ccd24989bd2a9b
vauxhall <zero.griffin@gmail.com>
2017-09-16 03:58:26 -0500

    Write socket replies

--- Source/Model/Connecting/Socket/MConnectingSocketTcpStrategies.swift
+++ Source/Model/Connecting/Socket/MConnectingSocketTcpStrategies.swift
@@ -2,6 +2,39 @@ import Foundation
 
 extension MConnectingSocketTcp
 {
+    //MARK: private
+    
+    private func socketWrite(string:String)
+    {
+        guard
+            
+            let data:Data = string.data(
+                using:String.Encoding.utf8,
+                allowLossyConversion:false)
+            
+        else
+        {
+            return
+        }
+        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.socketWrite(data:data)
+        }
+    }
+    
+    private func socketWrite(data:Data)
+    {
+        acceptedSocket?.write(
+            data,
+            withTimeout:0,
+            tag:0)
+        acceptedSocket?.readData(
+            withTimeout:0,
+            tag:0)
+    }
+    
     //MARK: internal
     
     func strategyShowpin()
@@ -18,21 +51,57 @@ extension MConnectingSocketTcp
     
     func strategyReplyOk()
     {
+        guard
         
+            let reply:String = model?.configuration.broadcast.replyOk
+        
+        else
+        {
+            return
+        }
+        
+        socketWrite(string:reply)
     }
     
     func strategyReplyPinError()
     {
+        guard
+            
+            let reply:String = model?.configuration.broadcast.replyPinError
+            
+        else
+        {
+            return
+        }
         
+        socketWrite(string:reply)
     }
     
     func strategyReplyConnected()
     {
+        guard
+            
+            let reply:String = model?.configuration.broadcast.replyConnected
+            
+        else
+        {
+            return
+        }
         
+        socketWrite(string:reply)
     }
     
     func strategyReplyConnectingError()
     {
+        guard
+            
+            let reply:String = model?.configuration.broadcast.replyConnectingError
+            
+        else
+        {
+            return
+        }
         
+        socketWrite(string:reply)
     }
 }
