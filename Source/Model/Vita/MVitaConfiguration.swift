f7f59d226bfec438a567cc3cf65c2a478f8d79cc
vauxhall <zero.griffin@gmail.com>
2017-09-16 02:36:07 -0500

    Add device to configuration

--- Source/Model/Vita/MVitaConfiguration.swift
+++ Source/Model/Vita/MVitaConfiguration.swift
@@ -3,6 +3,7 @@ import Foundation
 struct MVitaConfiguration
 {
     let broadcast:MVitaConfigurationBroadcast
+    let device:MVitaConfigurationDevice
     let lineSeparator:String
     let port:UInt16
 }
