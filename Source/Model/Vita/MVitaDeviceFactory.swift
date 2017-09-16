3d6f2b426243479e2e6a9a93a7407b846b71b85b
vauxhall <zero.griffin@gmail.com>
2017-09-16 02:19:23 -0500

    Create extension factory

---
+++ Source/Model/Vita/MVitaDeviceFactory.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension MVitaDevice
+{
+    //MARK: internal
+    
+    static func factoryDevice(
+        receivedString:String,
+        configuration:MVitaConfiguration)
+    {
+        
+    }
+}
