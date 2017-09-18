3ddd5f5dd307e3c784df6d67387cad1548164341
vauxhall <zero.griffin@gmail.com>
2017-09-18 10:43:26 -0500

    Create status protocol

--- Source/Model/Vita/MVitaLinkCommandDelegate.swift
+++ Source/Model/Vita/MVitaLinkCommandDelegate.swift
@@ -6,4 +6,21 @@ final class MVitaLinkCommandDelegate:
     GCDAsyncSocketDelegate
 {
     weak var model:MVitaLinkCommand?
+    
+    //MARK: delegate
+    
+    func socket(
+        _ sock:GCDAsyncSocket,
+        didConnectToHost host:String,
+        port:UInt16)
+    {
+    
+    }
+    
+    func socketDidDisconnect(
+        _ sock:GCDAsyncSocket,
+        withError err:Error?)
+    {
+        
+    }
 }
