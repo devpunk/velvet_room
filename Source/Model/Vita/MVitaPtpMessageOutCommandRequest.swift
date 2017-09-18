73d83c54605d4f7c897d014b9b9e7d59a5fd8062
vauxhall <zero.griffin@gmail.com>
2017-09-18 18:00:11 -0500

    Create type constants

--- Source/Model/Vita/MVitaPtpMessageOutCommandRequest.swift
+++ Source/Model/Vita/MVitaPtpMessageOutCommandRequest.swift
@@ -3,4 +3,12 @@ import Foundation
 struct MVitaPtpMessageOutCommandRequest:MVitaPtpMessageOutProtocol
 {
     let data:Data
+    
+    init()
+    {
+        let builder:MVitaPtpMessageOutBuilder = MVitaPtpMessageOutBuilder()
+        
+        
+        data = builder.export()
+    }
 }
---
+++ Source/Model/Vita/MVitaPtpType.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct MVitaPtpType
+{
+    static let commandRequest:UInt32 = 1
+}
