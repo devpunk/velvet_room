91b0d10a5c697b5c31ca3681d16b2a33d5943e48
vauxhall <zero.griffin@gmail.com>
2017-09-18 15:49:47 -0500

    Append pointer data

--- Source/Model/Vita/MVitaPtpMessage.swift
+++ Source/Model/Vita/MVitaPtpMessage.swift
@@ -2,5 +2,22 @@ import Foundation
 
 final class MVitaPtpMessage
 {
+    private(set) var data:Data
     
+    init()
+    {
+        data = Data()
+    }
+    
+    //MARK: internal
+    
+    func append<T>(value:T)
+    {
+        var value:T = value
+        let pointer:UnsafeBufferPointer = UnsafeBufferPointer(
+            start:&value,
+            count:1)
+        
+        data.append(pointer)
+    }
 }
