610472cf975179f662c235d30ba830719fd91499
vauxhall <zero.griffin@gmail.com>
2017-09-18 15:54:25 -0500

    Test ptp message

--- Source/Model/Home/MHome.swift
+++ Source/Model/Home/MHome.swift
@@ -4,4 +4,30 @@ final class MHome:Model<ArchHome>
 {
     var database:Database?
     var account:DAccount?
+    
+    required init()
+    {
+        super.init()
+        
+        let message:MVitaPtpMessage = MVitaPtpMessage()
+        let data:Data = message.data
+        
+        let array:[UInt32] = data.withUnsafeBytes
+        {
+            
+            Array(UnsafeBufferPointer<UInt32>(start: $0, count:1))
+        }
+        
+        print(array[0])
+    }
+    
+    func factoryPtp() -> MVitaPtpMessage
+    {
+        let firstVal:UInt32 = 55
+        
+        let message:MVitaPtpMessage = MVitaPtpMessage()
+        message.append(value:firstVal)
+        
+        return message
+    }
 }
