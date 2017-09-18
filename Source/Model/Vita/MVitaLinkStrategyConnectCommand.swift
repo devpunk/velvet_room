9fadec21659e99842c212666de4f6a9b6ee55815
vauxhall <zero.griffin@gmail.com>
2017-09-18 11:22:20 -0500

    Create connect command

---
+++ Source/Model/Vita/MVitaLinkStrategyConnectCommand.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+final class MVitaLinkStrategyConnectCommand:MVitaLinkStrategyProtocol
+{
+    private(set) var model:MVitaLink?
+    
+    init(model:MVitaLink)
+    {
+        self.model = model
+    }
+    
+    //MARK: protocol
+}
