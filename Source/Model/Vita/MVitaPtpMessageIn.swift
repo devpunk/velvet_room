a85931dc1853ddb22c44812ef5a7f1880d8ae4df
vauxhall <zero.griffin@gmail.com>
2017-09-18 17:01:17 -0500

    Parse message in

--- Source/Model/Vita/MVitaPtpMessageIn.swift
+++ Source/Model/Vita/MVitaPtpMessageIn.swift
@@ -2,5 +2,25 @@ import Foundation
 
 class MVitaPtpMessageIn
 {
+    let size:Int
+    let type:Int
     
+    init?(data:Data)
+    {
+        
+        
+        let headerInfo:[UInt32] = data.withUnsafeBytes
+        { (pointer:UnsafePointer<UInt32>) -> [UInt32] in
+            
+            let bufferPointer:UnsafeBufferPointer = UnsafeBufferPointer(
+                start:pointer,
+                count:2)
+            let array:[UInt32] = Array(bufferPointer)
+            
+            return array
+        }
+        
+        size = headerInfo[0]
+        type = headerInfo[1]
+    }
 }
