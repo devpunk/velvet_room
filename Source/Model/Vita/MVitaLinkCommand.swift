69fac6ba01d884ae28eec113f5c4805947db09c1
vauxhall <zero.griffin@gmail.com>
2017-09-20 03:21:12 -0500

    Subclass link socket

--- Source/Model/Vita/MVitaLinkCommand.swift
+++ Source/Model/Vita/MVitaLinkCommand.swift
@@ -1,62 +1,8 @@
 import Foundation
-import CocoaAsyncSocket
 
-final class MVitaLinkCommand
+final class MVitaLinkCommand:MVitaLinkSocket
 {
-    weak var model:MVitaLink?
-    let socket:GCDAsyncSocket
-    private let delegate:MVitaLinkCommandDelegate
-    private let queue:DispatchQueue
-    
-    init(
-        socket:GCDAsyncSocket,
-        delegate:MVitaLinkCommandDelegate,
-        queue:DispatchQueue)
-    {
-        self.socket = socket
-        self.delegate = delegate
-        self.queue = queue
-    }
-    
-    //MARK: private
-    
-    private func writeMessage(message:MVitaPtpMessageOutProtocol)
-    {
-        socket.write(
-            message.data,
-            withTimeout:0,
-            tag:0)
-        socket.readData(
-            withTimeout:0,
-            tag:0)
-    }
-    
     //MARK: internal
-    
-    func connect()
-    {
-        guard
-        
-            let model:MVitaLink = self.model
-        
-        else
-        {
-            return
-        }
-        
-        do
-        {
-            try socket.connect(
-                toHost:model.device.ipAddress,
-                onPort:model.device.port)
-        }
-        catch
-        {
-            let message:String = String.localizedModel(
-                key:"MVitaLinkCommand_errorConnect")
-            model.delegate?.linkError(message:message)
-        }
-    }
     
     func request()
     {
@@ -68,11 +14,5 @@ final class MVitaLinkCommand
     {
         let message:MVitaPtpMessageOutRequestCommand = MVitaPtpMessageOutRequestCommand()
         writeMessage(message:message)
-    }
-    
-    func cancel()
-    {
-        socket.delegate = nil
-        socket.disconnect()
     }
 }
--- Source/Model/Vita/MVitaLinkEvent.swift
+++ Source/Model/Vita/MVitaLinkEvent.swift
@@ -1,67 +1,13 @@
 import Foundation
-import CocoaAsyncSocket
 
-final class MVitaLinkEvent
+final class MVitaLinkEvent:MVitaLinkSocket
 {
-    weak var model:MVitaLink?
-    let socket:GCDAsyncSocket
-    private let delegate:MVitaLinkEventDelegate
-    private let queue:DispatchQueue
-    
-    init(
-        socket:GCDAsyncSocket,
-        delegate:MVitaLinkEventDelegate,
-        queue:DispatchQueue)
-    {
-        self.socket = socket
-        self.delegate = delegate
-        self.queue = queue
-    }
-    
     //MARK: internal
-    
-    func connect()
-    {
-        guard
-        
-            let model:MVitaLink = self.model
-        
-        else
-        {
-            return
-        }
-        
-        do
-        {
-            try socket.connect(
-                toHost:model.device.ipAddress,
-                onPort:model.device.port)
-        }
-        catch
-        {
-            let message:String = String.localizedModel(
-                key:"MVitaLinkEvent_errorConnect")
-            model.delegate?.linkError(message:message)
-        }
-    }
     
     func request(requestCommand:MVitaPtpMessageInRequestCommand)
     {
         let message:MVitaPtpMessageOutRequestEvent = MVitaPtpMessageOutRequestEvent(
             requestCommand:requestCommand)
-        
-        socket.write(
-            message.data,
-            withTimeout:0,
-            tag:0)
-        socket.readData(
-            withTimeout:0,
-            tag:0)
-    }
-    
-    func cancel()
-    {
-        socket.delegate = nil
-        socket.disconnect()
+        writeMessage(message:message)
     }
 }
