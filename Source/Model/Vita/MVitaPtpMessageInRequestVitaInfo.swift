8b85750fdf5547b504262fac352cb8a58ce47371
vauxhall <zero.griffin@gmail.com>
2017-09-20 14:40:59 -0500

    Create message in vita info

---
+++ Source/Model/Vita/MVitaPtpMessageInRequestVitaInfo.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+final class MVitaPtpMessageInRequestVitaInfo:MVitaPtpMessageIn
+{
+    let code:UInt16
+    let transactionId:UInt32
+    
+    override init?(
+        header:MVitaPtpMessageInHeader,
+        data:Data)
+    {
+        let codeSize:Int = MemoryLayout.size(ofValue:UInt16.self)
+        let transactionSize:Int = MemoryLayout.size(ofValue:UInt32.self)
+        let expectedSize:Int = codeSize + transactionSize
+        
+        guard
+            
+            data.count == expectedSize
+            
+            else
+        {
+            return nil
+        }
+        
+        let rangeTransaction:Range<Data.Index> = Range<Data.Index>(
+            codeSize ..< data.count)
+        let subdataTransaction:Data = data.subdata(in:rangeTransaction)
+        
+        guard
+            
+            let code:UInt16 = data.valueFromBytes(),
+            let transactionId:UInt32 = subdataTransaction.valueFromBytes()
+            
+            else
+        {
+            return nil
+        }
+        
+        self.code = code
+        self.transactionId = transactionId
+        
+        super.init(
+            header:header,
+            data:data)
+    }
+}
