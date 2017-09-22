012d5f29a8bf694d3b4261ccd0bea804ccfde9cc
vauxhall <zero.griffin@gmail.com>
2017-09-22 01:03:00 -0500

    Parse packet start

--- Source/Model/Vita/MVitaLinkStrategyReceiveData.swift
+++ Source/Model/Vita/MVitaLinkStrategyReceiveData.swift
@@ -4,13 +4,16 @@ class MVitaLinkStrategyReceiveData:MVitaLinkStrategyProtocol
 {
     var data:Data
     var payload:Int
+    var transactionId:UInt32
     private(set) var model:MVitaLink?
+    private let kElementsStart:Int = 2
     
     required init(model:MVitaLink)
     {
         self.model = model
         data = Data()
         payload = 0
+        transactionId = 0
     }
     
     //MARK: protocol
@@ -67,7 +70,22 @@ class MVitaLinkStrategyReceiveData:MVitaLinkStrategyProtocol
         header:MVitaPtpMessageInHeader,
         data:Data)
     {
+        guard
+            
+            let arrayData:[UInt32] = data.arrayFromBytes(
+                elements:kElementsStart),
+            let transactionId:UInt32 = arrayData.first,
+            let payloadUnsigned:UInt32 = arrayData.last
+            
+        else
+        {
+            failed()
+            
+            return
+        }
         
+        self.transactionId = transactionId
+        payload = Int(payloadUnsigned)
     }
     
     private func receivedPacket(
