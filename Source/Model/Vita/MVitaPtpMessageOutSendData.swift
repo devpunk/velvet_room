123fbcfe11e670ed2db7f417a9ad5f2b8886ceed
vauxhall <zero.griffin@gmail.com>
2017-09-23 01:30:50 -0500

    Create message out send data

---
+++ Source/Model/Vita/MVitaPtpMessageOutSendData.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct MVitaPtpMessageOutSendData:MVitaPtpMessageOutProtocol
+{
+    let data:Data
+    
+    init(code:UInt16)
+    {
+        let builder:MVitaPtpMessageOutBuilder = MVitaPtpMessageOutBuilder()
+        builder.append(value:MVitaPtpType.command)
+        builder.append(value:MVitaPtpDataPhase.send)
+        builder.append(value:code)
+        builder.appendTransactionId()
+        
+        data = builder.export()
+    }
+}
