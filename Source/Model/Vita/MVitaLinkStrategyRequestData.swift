bf4ad796ab27fd4d7d1e11c3d6c2c49cc659bd5f
vauxhall <zero.griffin@gmail.com>
2017-09-23 01:21:57 -0500

    Create request data

---
+++ Source/Model/Vita/MVitaLinkStrategyRequestData.swift
@@ -0,0 +1,197 @@
+import Foundation
+
+class MVitaLinkStrategyRequestData:MVitaLinkStrategyProtocol
+{
+    var data:Data
+    var payload:Int
+    var transactionId:UInt32
+    private(set) var model:MVitaLink?
+    private let kElementsStart:Int = 2
+    
+    required init(model:MVitaLink)
+    {
+        self.model = model
+        data = Data()
+        payload = 0
+        transactionId = 0
+    }
+    
+    //MARK: protocol
+    
+    func commandReceived(
+        header:MVitaPtpMessageInHeader,
+        data:Data)
+    {
+        switch header.type
+        {
+        case MVitaPtpType.dataPacketStart:
+            
+            receivedPacketStart(
+                header:header,
+                data:data)
+            
+            break
+            
+        case MVitaPtpType.dataPacket:
+            
+            receivedPacket(
+                header:header,
+                data:data)
+            
+            break
+            
+        case MVitaPtpType.dataPacketEnd:
+            
+            receivedPacketEnd(
+                header:header,
+                data:data)
+            
+            break
+            
+        case MVitaPtpType.commandAccepted:
+            
+            receivedConfirm(
+                header:header,
+                data:data)
+            
+            break
+            
+        default:
+            
+            failed()
+            
+            break
+        }
+    }
+    
+    //MARK: private
+    
+    private func readAgain()
+    {
+        DispatchQueue.global(
+            qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.model?.linkCommand.readData()
+        }
+    }
+    
+    private func removeTransaction(data:Data) -> Data?
+    {
+        guard
+            
+            let transactionId:UInt32 = data.valueFromBytes(),
+            self.transactionId == transactionId
+            
+        else
+        {
+            return nil
+        }
+        
+        let transactionSize:Int = MemoryLayout<UInt32>.size
+        let subdataRange:Range<Data.Index> = Range<Data.Index>(
+            transactionSize ..< data.count)
+        let subdata:Data = data.subdata(in:subdataRange)
+        
+        return subdata
+    }
+    
+    private func receivedPacketStart(
+        header:MVitaPtpMessageInHeader,
+        data:Data)
+    {
+        guard
+            
+            let arrayData:[UInt32] = data.arrayFromBytes(
+                elements:kElementsStart),
+            let transactionId:UInt32 = arrayData.first,
+            let payloadUnsigned:UInt32 = arrayData.last
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        self.transactionId = transactionId
+        payload = Int(payloadUnsigned)
+        
+        readAgain()
+    }
+    
+    private func receivedPacket(
+        header:MVitaPtpMessageInHeader,
+        data:Data)
+    {
+        guard
+            
+            let data:Data = removeTransaction(data:data)
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        self.data.append(data)
+        
+        readAgain()
+    }
+    
+    private func receivedPacketEnd(
+        header:MVitaPtpMessageInHeader,
+        data:Data)
+    {
+        guard
+            
+            let data:Data = removeTransaction(data:data)
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        self.data.append(data)
+        
+        guard
+            
+            self.data.count == payload
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        readAgain()
+    }
+    
+    private func receivedConfirm(
+        header:MVitaPtpMessageInHeader,
+        data:Data)
+    {
+        guard
+            
+            let code:UInt16 = data.valueFromBytes(),
+            code == MVitaPtpCommand.success
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        success()
+    }
+    
+    //MARK: internal
+    
+    func failed() { }
+    func success() { }
+}
