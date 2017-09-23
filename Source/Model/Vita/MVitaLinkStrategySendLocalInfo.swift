df94c95a1ea4b74615cf69b442a5c26dc74f12ce
vauxhall <zero.griffin@gmail.com>
2017-09-23 01:04:30 -0500

    Create strategy send local info

---
+++ Source/Model/Vita/MVitaLinkStrategySendLocalInfo.swift
@@ -0,0 +1,64 @@
+import Foundation
+import XmlHero
+
+final class MVitaLinkStrategySendLocalInfo:MVitaLinkStrategySendData
+{
+    override func failed()
+    {
+        let message:String = String.localizedModel(
+            key:"MVitaLinkStrategyRequestVitaInfo_messageFailed")
+        model?.delegate?.linkError(message:message)
+    }
+    
+    override func success()
+    {
+        Xml.object(data:data)
+        { [weak self] (xml:[String:Any]?, error:XmlError?) in
+            
+            if let error:XmlError = error
+            {
+                print("error")
+                print(error.localizedDescription)
+                
+                return
+            }
+            
+            guard
+                
+                let xml:[String:Any] = xml,
+                error == nil
+                
+                else
+            {
+                self?.failed()
+                
+                return
+            }
+            
+            print("xml")
+            print(xml)
+            
+            self?.vitaInfo(xml:xml)
+        }
+    }
+    
+    //MARK: private
+    
+    private func vitaInfo(xml:[String:Any])
+    {
+        guard
+            
+            let vitaInfo:MVitaInfo = MVitaInfo.factoryInfo(
+                xml:xml)
+            
+            else
+        {
+            failed()
+            
+            return
+        }
+        
+        model?.sendLocalInfo(vitaInfo:vitaInfo)
+    }
+}
+
