f39755a30109e1c2b69e96fbaf252ebed3206dc3
vauxhall <zero.griffin@gmail.com>
2017-09-23 15:23:59 -0500

    Add capabilities code

--- Source/Model/Vita/MVitaPtpCommand.swift
+++ Source/Model/Vita/MVitaPtpCommand.swift
@@ -6,4 +6,5 @@ struct MVitaPtpCommand
     static let success:UInt16 = 8193
     static let requestVitaInfo:UInt16 = 38161
     static let sendLocalInfo:UInt16 = 38172
+    static let requestVitaCapabilities:UInt16 = 38203
 }
