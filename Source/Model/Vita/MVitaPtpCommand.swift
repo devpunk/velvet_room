dae81cd62d108a8cb7d4473a6aa072be03c97ae5
vauxhall <zero.griffin@gmail.com>
2017-09-23 01:18:04 -0500

    Add send local info code

--- Source/Model/Vita/MVitaPtpCommand.swift
+++ Source/Model/Vita/MVitaPtpCommand.swift
@@ -5,4 +5,5 @@ struct MVitaPtpCommand
     static let openSession:UInt16 = 4098
     static let success:UInt16 = 8193
     static let requestVitaInfo:UInt16 = 38161
+    static let sendLocalInfo:UInt16 = 38172
 }
