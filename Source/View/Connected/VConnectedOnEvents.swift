da928125b33fd8f6663076e0c90f133492a45126
vauxhall <zero.griffin@gmail.com>
2017-09-24 16:44:32 -0500

    Create events views

---
+++ Source/View/Connected/VConnectedOnEvents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+final class VConnectedOnEvents:VCollection<
+    ArchConnected,
+    VConnectedOnEventsCell>
+{
+}
---
+++ Source/View/Connected/VConnectedOnEventsCell.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+final class VConnectedOnEventsCell:UICollectionViewCell
+{
+    
+}
