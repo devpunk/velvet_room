fbc71702c987f04833e0b9163a14f76ff07d18fb
vauxhall <zero.griffin@gmail.com>
2017-09-24 14:47:44 -0500

    Layout close view

--- Source/View/Connected/VConnectedOn.swift
+++ Source/View/Connected/VConnectedOn.swift
@@ -3,6 +3,8 @@ import UIKit
 final class VConnectedOn:View<ArchConnected>
 {
     private let kBarHeight:CGFloat = 64
+    private let kCloseBottom:CGFloat = -20
+    private let kCloseHeight:CGFloat = 120
     
     required init(controller:CConnected)
     {
@@ -24,7 +26,11 @@ final class VConnectedOn:View<ArchConnected>
         let viewBar:VConnectedOnBar = VConnectedOnBar(
             controller:controller)
         
+        let viewClose:VConnectedOnClose = VConnectedOnClose(
+            controller:controller)
+        
         addSubview(viewBar)
+        addSubview(viewClose)
         
         NSLayoutConstraint.topToTop(
             view:viewBar,
@@ -34,6 +40,17 @@ final class VConnectedOn:View<ArchConnected>
             constant:kBarHeight)
         NSLayoutConstraint.equalsHorizontal(
             view:viewBar,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:viewClose,
+            toView:self,
+            constant:kCloseBottom)
+        NSLayoutConstraint.height(
+            view:viewClose,
+            constant:kCloseHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewClose,
             toView:self)
     }
 }
