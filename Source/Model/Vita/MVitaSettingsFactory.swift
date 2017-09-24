164bc29863d5cc1b3fca765f6b320df0c89131e2
vauxhall <zero.griffin@gmail.com>
2017-09-24 04:09:47 -0500

    Factory settings

--- Source/Model/Vita/MVitaSettingsFactory.swift
+++ Source/Model/Vita/MVitaSettingsFactory.swift
@@ -4,20 +4,68 @@ extension MVitaSettings
 {
     //MARK: private
     
-    private static func factoryAccounts() -> [MVitaSettingsAccount]
+    private static func factoryAccounts(
+        xml:[String:Any]) -> [MVitaSettingsAccount]
     {
+        var accounts:[MVitaSettingsAccount] = []
         
+        guard
+        
+            let rawAccounts:[String:Any] = xml[
+                kKeyAccounts] as? [String:Any],
+            let rawDefault:[String:Any] = rawAccounts[
+                kKeyAccountDefault] as? [String:Any],
+            let accountDefault:MVitaSettingsAccount = factoryAccount(
+                xml:rawDefault)
+        
+        else
+        {
+            return accounts
+        }
+        
+        accounts.append(accountDefault)
+        
+        return accounts
     }
     
-    private static func factoryAccount() -> MVitaSettingsAccount?
+    private static func factoryAccount(
+        xml:[String:Any]) -> MVitaSettingsAccount?
     {
+        guard
         
+            let accountId:String = xml[
+                kKeyAccountId] as? String
+        
+        else
+        {
+            return nil
+        }
+        
+        let account:MVitaSettingsAccount = MVitaSettingsAccount(
+            accountId:accountId)
+        
+        return account
     }
     
     //MARK: internal
     
-    static func factorySettings() -> MVitaSettings?
+    static func factorySettings(xml:[String:Any]) -> MVitaSettings?
     {
+        guard
         
+            let rawSettings:[String:Any] = xml[
+                kKeySettings] as? [String:Any]
+        
+        else
+        {
+            return nil
+        }
+        
+        let accounts:[MVitaSettingsAccount] = factoryAccounts(
+            xml:rawSettings)
+        let settings:MVitaSettings = MVitaSettings(
+            accounts:accounts)
+        
+        return settings
     }
 }
--- Source/Model/Vita/MVitaSettingsKeys.swift
+++ Source/Model/Vita/MVitaSettingsKeys.swift
@@ -2,5 +2,8 @@ import Foundation
 
 extension MVitaSettings
 {
-    
+    static let kKeySettings:String = "settingInfo"
+    static let kKeyAccounts:String = "accounts"
+    static let kKeyAccountDefault:String = "npAccount"
+    static let kKeyAccountId:String = "accountId"
 }
