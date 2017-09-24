506bfbda8b0f5415a06d200981f1f18be31f7153
vauxhall <zero.griffin@gmail.com>
2017-09-24 01:38:26 -0500

    Rename as send local status

--- Source/Model/Vita/MVitaPtpMessageOutSendLocalStatus.swift
+++ Source/Model/Vita/MVitaPtpMessageOutSendLocalStatus.swift
@@ -1,18 +1,18 @@
 import Foundation
 
-struct MVitaPtpMessageOutLocalStatus:MVitaPtpMessageOutProtocol
+struct MVitaPtpMessageOutSendLocalStatus:MVitaPtpMessageOutProtocol
 {
     let data:Data
     private let kSessionId:UInt32 = 1
     
-    init()
+    init(status:MVitaPtpLocalStatus)
     {
         let builder:MVitaPtpMessageOutBuilder = MVitaPtpMessageOutBuilder()
         builder.append(value:MVitaPtpType.command.rawValue)
         builder.append(value:MVitaPtpDataPhase.none.rawValue)
-        builder.append(value:MVitaPtpCommand.openSession.rawValue)
+        builder.append(value:MVitaPtpCommand.sendLocalStatus.rawValue)
         builder.appendTransactionId()
-        builder.append(value:kSessionId)
+        builder.append(value:status.rawValue)
         
         data = builder.export()
     }
