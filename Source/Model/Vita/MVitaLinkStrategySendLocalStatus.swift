8d299f12e18bbd43cc62b112b2c4596a56430b17
vauxhall <zero.griffin@gmail.com>
2017-09-24 01:39:46 -0500

    Create strategy send local status

---
+++ Source/Model/Vita/MVitaLinkStrategySendLocalStatus.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+final class MVitaLinkStrategySendLocalStatus:MVitaLinkStrategyProtocol
+{
+    private(set) var model:MVitaLink?
+    
+    init(model:MVitaLink)
+    {
+        self.model = model
+    }
+    
+    //MARK: protocol
+    
+    func commandReceived(
+        header:MVitaPtpMessageInHeader,
+        data:Data)
+    {
+        guard
+            
+            let requestCommand:MVitaPtpMessageInRequestCommand = MVitaPtpMessageInRequestCommand(
+                header:header,
+                data:data),
+            header.type == MVitaPtpType.commandRequestAccepted
+            
+            else
+        {
+            failed()
+            
+            return
+        }
+        
+        success(requestCommand:requestCommand)
+    }
+    
+    //MARK: private
+    
+    private func failed()
+    {
+        let message:String = String.localizedModel(
+            key:"MVitaLinkStrategyRequestCommand_messageFailed")
+        model?.delegate?.linkError(message:message)
+    }
+    
+    private func success(
+        requestCommand:MVitaPtpMessageInRequestCommand)
+    {
+        model?.requestEvent(requestCommand:requestCommand)
+    }
+}
