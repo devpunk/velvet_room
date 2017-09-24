ff0aad2cb9480935b671521d51ae39a5a6557b0b
vauxhall <zero.griffin@gmail.com>
2017-09-24 05:50:35 -0500

    Update status

--- Source/View/Connected/VConnectedStatus.swift
+++ Source/View/Connected/VConnectedStatus.swift
@@ -11,23 +11,19 @@ extension VConnected
         guard
             
             let controller:CConnected = self.controller as? CConnected,
-            let status:MConnectingStatusProtocol = controller.model.status
+            let status:MConnectedStatusProtocol = controller.model.status
             
         else
         {
             return
         }
         
-        button.setTitle(
-            status.buttonTitle,
-            for:UIControlState.normal)
-        
-        let viewType:View<ArchConnecting>.Type = status.viewType
-        let viewStatus:View<ArchConnecting> = viewType.init(
+        let viewType:View<ArchConnected>.Type = status.viewType
+        let viewStatus:View<ArchConnected> = viewType.init(
             controller:controller)
         self.viewStatus = viewStatus
         
-        insertSubview(viewStatus, belowSubview:button)
+        addSubview(viewStatus)
         
         NSLayoutConstraint.equals(
             view:viewStatus,
