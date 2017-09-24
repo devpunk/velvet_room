9c06729e14b4b3628d8f1dc01873970247147997
vauxhall <zero.griffin@gmail.com>
2017-09-23 19:41:37 -0500

    Use local status as enum

--- Source/Model/Vita/MVitaPtpLocalStatus.swift
+++ Source/Model/Vita/MVitaPtpLocalStatus.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-struct MVitaPtpLocalStatus
+enum MVitaPtpLocalStatus:UInt32
 {
-    static let connection:UInt32 = 0
-    static let unknown:UInt32 = 1
-    static let deactive:UInt32 = 2
-    static let connectionEnd:UInt32 = 3
-    static let connectionStart:UInt32 = 4
-    static let unknownOther:UInt32 = 5
+    case connection = 0
+    case unknown = 1
+    case deactive = 2
+    case connectionEnd = 3
+    case connectionStart = 4
+    case unknownOther = 5
 }
