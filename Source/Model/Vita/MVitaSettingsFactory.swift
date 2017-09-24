e08f15d9e47dc0b3023acd55256039d611ff1212
vauxhall <zero.griffin@gmail.com>
2017-09-24 04:01:15 -0500

    Fix typo in name

---
+++ Source/Model/Vita/MVitaSettingsFactory.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+extension MVitaSettings
+{
+    //MARK: private
+    
+    private static func factoryAccounts() -> [MVitaSettingsAccount]
+    {
+        
+    }
+    
+    private static func factoryAccount() -> MVitaSettingsAccount?
+    {
+        
+    }
+    
+    //MARK: internal
+    
+    static func factorySettings() -> MVitaSettings?
+    {
+        
+    }
+}
