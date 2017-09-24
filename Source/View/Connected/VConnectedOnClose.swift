30d283ead4223de2feab82858896969933efc544
vauxhall <zero.griffin@gmail.com>
2017-09-24 14:19:21 -0500

    Create view close

---
+++ Source/View/Connected/VConnectedOnClose.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class VConnectedOnClose:View<ArchConnected>
+{
+    required init(controller:CConnected)
+    {
+        super.init(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
