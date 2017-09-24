86a714cdb38828d945e88ff909c04343773d715e
vauxhall <zero.griffin@gmail.com>
2017-09-24 05:38:18 -0500

    Create connected status protocol

---
+++ Source/Model/Connected/MConnectedStatusProtocol.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+protocol MConnectedStatusProtocol
+{
+    var viewType:View<ArchConnected>.Type { get }
+}
--- Source/View/Connected/VConnected.swift
+++ Source/View/Connected/VConnected.swift
@@ -2,5 +2,16 @@ import UIKit
 
 final class VConnected:ViewMain
 {
+    weak var viewStatus:View<ArchConnected>?
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        updateStatus()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
