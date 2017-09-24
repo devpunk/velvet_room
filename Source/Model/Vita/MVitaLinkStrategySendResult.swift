2a5a41971aebc6068aac29e560b594aae7772070
vauxhall <zero.griffin@gmail.com>
2017-09-24 04:17:27 -0500

    Create strategy send result

---
+++ Source/Model/Vita/MVitaLinkStrategySendResult.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+final class MVitaLinkStrategySendResult:MVitaLinkStrategyProtocol
+{
+    private(set) var model:MVitaLink?
+    
+    init(model:MVitaLink)
+    {
+        self.model = model
+    }
+    
+    //MARK: protocol
+    
+    func commandReceived(
+        header:MVitaPtpMessageInHeader,
+        data:Data)
+    {
+        guard
+            
+            let confirm:MVitaPtpMessageInConfirm = MVitaPtpMessageInConfirm(
+                header:header,
+                data:data),
+            header.type == MVitaPtpType.commandAccepted,
+            confirm.code == MVitaPtpCommand.success
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        success()
+    }
+    
+    //MARK: private
+    
+    private func failed()
+    {
+        let message:String = String.localizedModel(
+            key:"MVitaLinkStrategySendResult_messageFailed")
+        model?.delegate?.linkError(message:message)
+    }
+    
+    private func success()
+    {
+        model?.listenEvents()
+    }
+}
