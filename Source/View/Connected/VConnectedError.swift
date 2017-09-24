46abb378c0d0b258a4cf066710e57687f2923af3
vauxhall <zero.griffin@gmail.com>
2017-09-24 06:13:51 -0500

    Create view error

---
+++ Source/View/Connected/VConnectedError.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+final class VConnectedError:View<ArchConnected>
+{
+    required init(controller:CConnected)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.colourBackgroundGray
+        
+        factoryViews()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews()
+    {
+        
+    }
+}
