10dec9ed1082a64d4cfcf3b6a0b0fd2fbccccc87
vauxhall <zero.griffin@gmail.com>
2017-09-23 19:13:09 -0500

    Create constants for local status

---
+++ Source/Model/Vita/MVitaPtpLocalStatus.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct MVitaPtpLocalStatus
+{
+    static let connection:UInt32 = 0
+    static let unknown:UInt32 = 1
+    static let deactive:UInt32 = 2
+    static let connectionEnd:UInt32 = 3
+    static let connectionStart:UInt32 = 4
+    static let unknownOther:UInt32 = 5
+}
