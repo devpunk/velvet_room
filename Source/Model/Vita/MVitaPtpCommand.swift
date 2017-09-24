32bae191343b2f4e0c2b375a528c089497ff441a
vauxhall <zero.griffin@gmail.com>
2017-09-23 19:28:24 -0500

    Fix capabilities xml

--- Source/Model/Vita/MVitaPtpCommand.swift
+++ Source/Model/Vita/MVitaPtpCommand.swift
@@ -4,6 +4,7 @@ struct MVitaPtpCommand
 {
     static let openSession:UInt16 = 4098
     static let success:UInt16 = 8193
+    static let error:UInt16 = 8194
     static let requestVitaInfo:UInt16 = 38161
     static let sendLocalInfo:UInt16 = 38172
     static let requestVitaCapabilities:UInt16 = 38203
