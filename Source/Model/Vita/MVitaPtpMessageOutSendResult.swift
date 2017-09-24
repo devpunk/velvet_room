d9a3b59754e24458d1c10dab56759f80948c8d99
vauxhall <zero.griffin@gmail.com>
2017-09-24 04:26:37 -0500

    Create message send result

---
+++ Source/Model/Vita/MVitaPtpMessageOutSendResult.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct MVitaPtpMessageOutSendResult:MVitaPtpMessageOutProtocol
+{
+    let data:Data
+    
+    init(
+        event:MVitaPtpMessageInEvent,
+        result:MVitaPtpResult)
+    {
+        let builder:MVitaPtpMessageOutBuilder = MVitaPtpMessageOutBuilder()
+        builder.append(value:MVitaPtpType.command.rawValue)
+        builder.append(value:MVitaPtpDataPhase.none.rawValue)
+        builder.append(value:MVitaPtpCommand.sendResult.rawValue)
+        builder.appendTransactionId()
+        builder.append(value:event.eventId)
+        builder.append(value:result.rawValue)
+        
+        data = builder.export()
+    }
+}
