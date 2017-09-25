c1364ea4e472826943f881709e8fa8262061a9b9
vauxhall <zero.griffin@gmail.com>
2017-09-25 07:50:50 -0500

    Layout view base

--- Source/View/Connected/VConnectedOnEventsCell.swift
+++ Source/View/Connected/VConnectedOnEventsCell.swift
@@ -2,6 +2,48 @@ import UIKit
 
 final class VConnectedOnEventsCell:UICollectionViewCell
 {
+    private let kBaseMargin:CGFloat = 2
+    private let kBaseBottom:CGFloat = -32
+    private let kCornerRadius:CGFloat = 8
+    private let kBorderWidth:CGFloat = 1
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        backgroundColor = UIColor.clear
+        clipsToBounds = true
+        isUserInteractionEnabled = false
+        
+        let viewBase:UIView = UIView()
+        viewBase.isUserInteractionEnabled = false
+        viewBase.translatesAutoresizingMaskIntoConstraints = false
+        viewBase.clipsToBounds = true
+        viewBase.layer.cornerRadius = kCornerRadius
+        viewBase.layer.borderWidth = kBorderWidth
+        viewBase.layer.borderColor = UIColor(
+            white:0, alpha:0.1).cgColor
+        
+        addSubview(viewBase)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBase,
+            toView:self,
+            constant:kBaseMargin)
+        NSLayoutConstraint.bottomToBottom(
+            view:viewBase,
+            toView:self,
+            constant:kBaseBottom)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBase,
+            toView:self,
+            margin:kBaseMargin)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
     //MARK: internal
     
     func config(model:MConnectedEvent)
