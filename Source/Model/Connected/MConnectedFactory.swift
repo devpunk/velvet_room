5379d4b324cd2fc979fa9ead9981bd9db948c386
vauxhall <zero.griffin@gmail.com>
2017-09-25 10:58:44 -0500

    Add system icon

--- Source/Model/Connected/MConnectedFactory.swift
+++ Source/Model/Connected/MConnectedFactory.swift
@@ -1,16 +1,72 @@
-import Foundation
+import UIKit
 
 extension MConnected
 {
     //MARK: private
     
     private class func factoryEvent(
-        log:MVitaLinkLogProtocol) -> MConnectedEventProtocol
+        log:MVitaLinkLogProtocol) -> MConnectedEventProtocol?
+    {
+        switch log.logType
+        {
+        case MVitaLinkLogType.system:
+            
+            let event:MConnectedEventProtocol? = factoryEvent(
+                logSystem:log)
+            
+            return event
+        
+        case MVitaLinkLogType.gameSave:
+            
+            let event:MConnectedEventProtocol? = factoryEvent(
+                logGameSave:log)
+            
+            return event
+        }
+    }
+    
+    private class func factoryEvent(
+        logSystem:MVitaLinkLogProtocol) -> MConnectedEventProtocol?
+    {
+        guard
+        
+            let logSystem:MVitaLinkLogSystem = logSystem as? MVitaLinkLogSystem
+        
+        else
+        {
+            return nil
+        }
+        
+        let icon:UIImage = 
+        let event:MConnectedEventIcon = MConnectedEventIcon(
+            icon: <#T##UIImage#>,
+            title: <#T##String#>,
+            timestamp:logSystem.timestamp)
+    }
+    
+    private class func factoryEvent(
+        logGameSave:MVitaLinkLogProtocol) -> MConnectedEventProtocol?
     {
         
     }
     
-    
+    private class func factoryTitle(
+        logSystemType:MVitaLinkLogSystemType) -> String
+    {
+        let title:String
+        
+        switch logSystemType
+        {
+        case MVitaLinkLogSystemType.connectionStart:
+            
+            title = String.localizedModel(key:
+            "MConnected_eventSystemConnectionStart")
+            
+            break
+        }
+        
+        return title
+    }
     
     //MARK: internal
     
@@ -21,8 +77,15 @@ extension MConnected
         
         for log:MVitaLinkLogProtocol in logs
         {
-            let event:MConnectedEventProtocol = factoryEvent(
-                log:log)
+            guard
+                
+                let event:MConnectedEventProtocol = factoryEvent(
+                    log:log)
+            
+            else
+            {
+                continue
+            }
             
             events.append(event)
         }
