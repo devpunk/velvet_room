867f82ab8f3eab1c839a17d8c1ec97db27caee8e
vauxhall <zero.griffin@gmail.com>
2017-09-26 03:52:25 -0500

    Layout image view

--- Source/View/Connected/VConnectedOnEventsCellIcon.swift
+++ Source/View/Connected/VConnectedOnEventsCellIcon.swift
@@ -2,8 +2,50 @@ import UIKit
 
 final class VConnectedOnEventsCellIcon:VConnectedOnEventsCell
 {
+    private weak var imageView:UIImageView!
+    
+    override init(frame:CGRect)
+    {
+        let imageViewHeight:CGFloat = frame.width
+        
+        super.init(frame:frame)
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:imageView,
+            constant:imageViewHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
     override func config(model:MConnectedEventProtocol)
     {
-        NSLocalizedString("MConnectedWalk", comment:"")
+        guard
+        
+            let model:MConnectedEventIcon = model as? MConnectedEventIcon
+        
+        else
+        {
+            return
+        }
+        
+        imageView.image = model.icon
     }
 }
