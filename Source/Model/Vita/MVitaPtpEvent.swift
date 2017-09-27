e312cb3c1c57f86aeece247a455e49c489012c43
vauxhall <zero.griffin@gmail.com>
2017-09-27 06:06:07 -0500

    Rename to request object status

--- Source/Model/Vita/MVitaPtpEvent.swift
+++ Source/Model/Vita/MVitaPtpEvent.swift
@@ -1,7 +1,7 @@
 enum MVitaPtpEvent:UInt16
 {
     case unknown
-    case sendObjectStatus = 49423
+    case requestObjectStatus = 49423
     case requestSettings = 49426
     case sendStorageSize = 49433
 }
