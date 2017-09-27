9a83de9978a2bb620690729f55ebf7216e951832
vauxhall <zero.griffin@gmail.com>
2017-09-27 11:41:26 -0500

    Add case terminate

--- Source/Model/Vita/MVitaPtpEvent.swift
+++ Source/Model/Vita/MVitaPtpEvent.swift
@@ -1,7 +1,8 @@
 enum MVitaPtpEvent:UInt16
 {
     case unknown
-    case requestItemStatus            = 49423
+    case requestItemStatus              = 49423
     case requestSettings                = 49426
     case sendStorageSize                = 49433
+    case requestTerminate               = 49446
 }
