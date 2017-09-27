4e853bb3659c10baf49d262cc7ca61ae0882e1df
vauxhall <zero.griffin@gmail.com>
2017-09-27 17:13:18 -0500

    Add case request treat item

--- Source/Model/Vita/MVitaPtpEvent.swift
+++ Source/Model/Vita/MVitaPtpEvent.swift
@@ -4,5 +4,6 @@ enum MVitaPtpEvent:UInt16
     case requestItemStatus              = 49423
     case requestSettings                = 49426
     case sendStorageSize                = 49433
+    case requestTreatItem               = 49442
     case terminate                      = 49446
 }
