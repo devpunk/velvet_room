718151a2f4574c065f5f7ff39624a624e6139fba
vauxhall <zero.griffin@gmail.com>
2017-09-27 17:19:48 -0500

    Rename as item treat

--- Source/Model/Vita/MVitaPtpEvent.swift
+++ Source/Model/Vita/MVitaPtpEvent.swift
@@ -4,6 +4,6 @@ enum MVitaPtpEvent:UInt16
     case requestItemStatus              = 49423
     case requestSettings                = 49426
     case sendStorageSize                = 49433
-    case requestTreatItem               = 49442
+    case requestItemTreat               = 49442
     case terminate                      = 49446
 }
