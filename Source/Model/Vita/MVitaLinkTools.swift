034f5daf42949877a56547e527b88777b9a76440
vauxhall <zero.griffin@gmail.com>
2017-09-27 05:50:49 -0500

    Add wrap function

--- Source/Model/Vita/MVitaLinkTools.swift
+++ Source/Model/Vita/MVitaLinkTools.swift
@@ -4,5 +4,13 @@ extension MVitaLink
 {
     //MARK: internal
     
-    
+    class func wrapDataWithSizeHeader(data:Data) -> Data
+    {
+        let size:UInt32 = UInt32(data.count)
+        var dataWrapped:Data = Data()
+        dataWrapped.append(value:size)
+        dataWrapped.append(data)
+        
+        return dataWrapped
+    }
 }
