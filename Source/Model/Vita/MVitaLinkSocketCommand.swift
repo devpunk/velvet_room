08eab58fc981658bdbcd63b6ddd38b44db9c7e3b
vauxhall <zero.griffin@gmail.com>
2017-09-27 17:25:01 -0500

    Migrate event methods

--- Source/Model/Vita/MVitaLinkSocketCommand.swift
+++ Source/Model/Vita/MVitaLinkSocketCommand.swift
@@ -39,36 +39,6 @@ final class MVitaLinkSocketCommand:MVitaLinkSocket
         writeMessageAndRead(message:message)
     }
     
-    func requestSettings(
-        event:MVitaPtpMessageInEvent)
-    {
-        let message:MVitaPtpMessageOutEventCommand = MVitaPtpMessageOutEventCommand(
-            event:event,
-            dataPhase:MVitaPtpDataPhase.request,
-            command:MVitaPtpCommand.requestSettings)
-        writeMessageAndRead(message:message)
-    }
-    
-    func requestItemStatus(
-        event:MVitaPtpMessageInEvent)
-    {
-        let message:MVitaPtpMessageOutEventCommand = MVitaPtpMessageOutEventCommand(
-            event:event,
-            dataPhase:MVitaPtpDataPhase.request,
-            command:MVitaPtpCommand.requestItemStatus)
-        writeMessageAndRead(message:message)
-    }
-    
-    func sendResult(
-        event:MVitaPtpMessageInEvent,
-        result:MVitaPtpResult)
-    {
-        let message:MVitaPtpMessageOutSendResult = MVitaPtpMessageOutSendResult(
-            event:event,
-            result:result)
-        writeMessageAndRead(message:message)
-    }
-    
     func closeSession()
     {
         let message:MVitaPtpMessageOutGenericCommand = MVitaPtpMessageOutGenericCommand(
--- Source/Model/Vita/MVitaLinkSocketCommandEvent.swift
+++ Source/Model/Vita/MVitaLinkSocketCommandEvent.swift
@@ -3,4 +3,34 @@ import Foundation
 extension MVitaLinkSocketCommand
 {
     //MARK: internal
+    
+    func requestSettings(
+        event:MVitaPtpMessageInEvent)
+    {
+        let message:MVitaPtpMessageOutEventCommand = MVitaPtpMessageOutEventCommand(
+            event:event,
+            dataPhase:MVitaPtpDataPhase.request,
+            command:MVitaPtpCommand.requestSettings)
+        writeMessageAndRead(message:message)
+    }
+    
+    func requestItemStatus(
+        event:MVitaPtpMessageInEvent)
+    {
+        let message:MVitaPtpMessageOutEventCommand = MVitaPtpMessageOutEventCommand(
+            event:event,
+            dataPhase:MVitaPtpDataPhase.request,
+            command:MVitaPtpCommand.requestItemStatus)
+        writeMessageAndRead(message:message)
+    }
+    
+    func sendResult(
+        event:MVitaPtpMessageInEvent,
+        result:MVitaPtpResult)
+    {
+        let message:MVitaPtpMessageOutSendResult = MVitaPtpMessageOutSendResult(
+            event:event,
+            result:result)
+        writeMessageAndRead(message:message)
+    }
 }
