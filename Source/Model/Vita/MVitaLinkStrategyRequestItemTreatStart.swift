2a0b7b81bb6ff91bbada271c7a018623b135ed17
vauxhall <zero.griffin@gmail.com>
2017-09-27 19:34:47 -0500

    Create treat start

---
+++ Source/Model/Vita/MVitaLinkStrategyRequestItemTreatStart.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+final class MVitaLinkStrategyRequestItemTreatStart:MVitaLinkStrategyRequestItemTreatProtocol
+{
+    func success(
+        strategy:MVitaLinkStrategyRequestItemTreat)
+    {
+        
+    }
+}
