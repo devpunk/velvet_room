424c588e7a21a317ca41b75f64f31f4fed18ce3b
vauxhall <zero.griffin@gmail.com>
2017-09-28 09:51:32 -0500

    Layout error bar

--- Source/View/Connected/VConnectedError.swift
+++ Source/View/Connected/VConnectedError.swift
@@ -2,6 +2,8 @@ import UIKit
 
 final class VConnectedError:View<ArchConnected>
 {
+    private let kBarHeight:CGFloat = 200
+    
     required init(controller:CConnected)
     {
         super.init(controller:controller)
@@ -18,6 +20,19 @@ final class VConnectedError:View<ArchConnected>
     
     private func factoryViews()
     {
+        let viewBar:VConnectedErrorBar = VConnectedErrorBar(
+            controller:controller)
         
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
     }
 }
--- Source/View/Connected/VConnectedErrorBar.swift
+++ Source/View/Connected/VConnectedErrorBar.swift
@@ -2,5 +2,35 @@ import UIKit
 
 final class VConnectedErrorBar:View<ArchConnected>
 {
+    private let kContentTop:CGFloat = 20
     
+    required init(controller:CConnected)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = #imageLiteral(resourceName: "assetConnectError")
+        addSubview(imageView)
+        
+        NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self,
+            constant:kContentTop)
+        NSLayoutConstraint.bottomToBottom(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.equalsHorizontal(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
