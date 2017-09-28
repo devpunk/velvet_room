377b84eca77b7b81cfa51e33c9019d784f5ca185
vauxhall <zero.griffin@gmail.com>
2017-09-27 21:45:10 -0500

    Create message item property

---
+++ Source/Model/Vita/MVitaPtpMessageOutItemProperty.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct MVitaPtpMessageOutItemProperty:MVitaPtpMessageOutProtocol
+{
+    let data:Data
+    
+    init(
+        itemTreat:MVitaItemTreat,
+        dataPhase:MVitaPtpDataPhase,
+        property:MVitaPtpItemProperty)
+    {
+        let builder:MVitaPtpMessageOutBuilder = MVitaPtpMessageOutBuilder()
+        builder.append(value:MVitaPtpType.command.rawValue)
+        builder.append(value:dataPhase.rawValue)
+        builder.append(
+            value:MVitaPtpCommand.requestItemPropertyValue.rawValue)
+        builder.appendTransactionId()
+        builder.append(value:itemTreat.treatId)
+        builder.append(value:property.rawValue)
+        
+        data = builder.export()
+    }
+}
