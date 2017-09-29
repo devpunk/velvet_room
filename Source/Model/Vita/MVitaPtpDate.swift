e10b18f37705a6bdf11f53929f28418c13857459
vauxhall <zero.griffin@gmail.com>
2017-09-29 07:02:00 -0500

    Factory date

--- Source/Model/Vita/MVitaPtpDate.swift
+++ Source/Model/Vita/MVitaPtpDate.swift
@@ -2,9 +2,44 @@ import Foundation
 
 final class MVitaPtpDate
 {
+    private static let kDateFormat:String = "yyyyMMdd'T'HH:mm:ss.SSS"
+    
     //MARK: private
     
     private init() { }
     
+    private class func factoryDateFormatter() -> DateFormatter
+    {
+        let dateFormatter:DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = kDateFormat
+        
+        return dateFormatter
+    }
+    
+    private class func factoryDate(string:String) -> Date?
+    {
+        let dateFormatter:DateFormatter = factoryDateFormatter()
+        let date:Date? = dateFormatter.date(from:string)
+        
+        return date
+    }
+    
     //MARK: internal
+    
+    class func factoryDate(data:Data) -> Date?
+    {
+        guard
+        
+            let string:String = MVitaPtpString.factoryString(
+                data:data)
+        
+        else
+        {
+            return nil
+        }
+        
+        let date:Date? = factoryDate(string:string)
+        
+        return date
+    }
 }
