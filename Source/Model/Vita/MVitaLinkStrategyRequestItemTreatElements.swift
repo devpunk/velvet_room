55d0d4c2acc669f07e5a022205b66faa19141759
vauxhall <zero.griffin@gmail.com>
2017-09-29 08:14:18 -0500

    Parse item elements

--- Source/Model/Vita/MVitaLinkStrategyRequestItemTreatElements.swift
+++ Source/Model/Vita/MVitaLinkStrategyRequestItemTreatElements.swift
@@ -5,6 +5,34 @@ final class MVitaLinkStrategyRequestItemTreatElements:MVitaLinkStrategyRequestIt
     func success(
         strategy:MVitaLinkStrategyRequestItemTreat)
     {
+        let data:Data = strategy.data
+        let counterSize:Int = MemoryLayout<UInt32>.size
         
+        guard
+        
+            let rawCountElements:UInt32 = data.valueFromBytes()
+        
+        else
+        {
+            strategy.failed()
+            
+            return
+        }
+        
+        let countElements:Int = Int(rawCountElements)
+        let subData:Data = data.subdata(
+            start:counterSize)
+        
+        guard
+        
+            let elements:[UInt32] = subData.arrayFromBytes(
+                elements:countElements)
+        
+        else
+        {
+            strategy.failed()
+            
+            return
+        }
     }
 }
