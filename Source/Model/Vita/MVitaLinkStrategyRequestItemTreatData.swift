cfd01cf59d3a4373d9a5c7035e852ba754c6363a
vauxhall <zero.griffin@gmail.com>
2017-09-29 12:37:39 -0500

    Create strategy data

---
+++ Source/Model/Vita/MVitaLinkStrategyRequestItemTreatData.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+final class MVitaLinkStrategyRequestItemTreatData:MVitaLinkStrategyRequestItemTreatProtocol
+{
+    //MARK: internal
+    
+    func success(
+        strategy:MVitaLinkStrategyRequestItemTreat)
+    {
+        print("item data size: \(strategy.data.count)")
+    }
+}
