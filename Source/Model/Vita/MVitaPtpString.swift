c157b78b2e0089dc30f731f4625faab173217b5f
vauxhall <zero.griffin@gmail.com>
2017-09-28 20:04:48 -0500

    Create ptp string factory

---
+++ Source/Model/Vita/MVitaPtpString.swift
@@ -0,0 +1,82 @@
+import Foundation
+
+final class MVitaPtpString
+{
+    private static let kBytesForSize:Int = 1
+    private static let kBytesPerCharacter:Int = 2
+    private static let kWrappingCharacter:UInt8 = 0
+    
+    //MARK: private
+    
+    private init() { }
+    
+    private class func factoryLength(
+        data:Data) -> Int?
+    {
+        guard
+            
+            let size:UInt8 = data.valueFromBytes()
+            
+        else
+        {
+            return nil
+        }
+        
+        let sizeInt:Int = Int(size)
+        let totalSize:Int = sizeInt * kBytesPerCharacter
+        
+        return totalSize
+    }
+    
+    private class func factoryStringConfirm(
+        data:Data) -> String?
+    {
+        let wrappedData:Data = wrapData(data:data)
+        
+        guard
+            
+            let string:String = String(
+                data:wrappedData,
+                encoding:String.Encoding.utf16)
+            
+        else
+        {
+            return nil
+        }
+        
+        return string
+    }
+    
+    private class func wrapData(data:Data) -> Data
+    {
+        let subData:Data = data.subdata(
+            start:kBytesForSize)
+        
+        var wrappedData:Data = Data()
+        wrappedData.append(value:kWrappingCharacter)
+        wrappedData.append(subData)
+        wrappedData.append(value:kWrappingCharacter)
+        
+        return data
+    }
+    
+    //MARK: internal
+    
+    class func factoryString(
+        data:Data) -> String?
+    {
+        guard
+            
+            let length:Int = factoryLength(data:data),
+            length > data.count,
+            let string:String = factoryStringConfirm(
+                data:data)
+        
+        else
+        {
+            return nil
+        }
+        
+        return string
+    }
+}
