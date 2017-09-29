0224a603ab2a688d55203a76715724595485d0d9
vauxhall <zero.griffin@gmail.com>
2017-09-29 13:10:42 -0500

    Create extension for files

---
+++ Source/Model/Vita/MVitaLinkStrategyRequestItemTreatStatus.swift
@@ -0,0 +1,190 @@
+import Foundation
+
+extension MVitaLinkStrategyRequestItemTreat
+{
+    //MARK: private
+    
+    private func requestItem(
+        rawElement:UInt32)
+    {
+        guard
+            
+            let parentTreat:MVitaItemTreat = currentItem?.treat
+            
+        else
+        {
+            return
+        }
+        
+        let itemTreat:MVitaItemTreat = MVitaItemTreat.factoryTreat(
+            parentTreat:parentTreat,
+            treatId:rawElement)
+        
+        requestItemFormat(itemTreat:itemTreat)
+    }
+    
+    private func createNewItem(
+        itemTreat:MVitaItemTreat)
+    {
+        let newItem:MVitaItemIn = MVitaItemIn(
+            treat:itemTreat,
+            parent:currentItem)
+        currentItem?.addElement(element:newItem)
+        currentItem = newItem
+        
+        if rootItemIn == nil
+        {
+            rootItemIn = currentItem
+        }
+    }
+    
+    private func nextElement()
+    {
+        guard
+            
+            let rawElement:UInt32 = currentItem?.rawElements?.popLast()
+            
+        else
+        {
+            currentItemFinished()
+            
+            return
+        }
+        
+        requestItem(rawElement:rawElement)
+    }
+    
+    private func currentItemFinished()
+    {
+        guard
+            
+            let parent:MVitaItemIn = currentItem?.parent
+            
+        else
+        {
+            allItemsFinished()
+            
+            return
+        }
+        
+        currentItem = parent
+        nextElement()
+    }
+    
+    private func allItemsFinished()
+    {
+        print("finished all items")
+    }
+    
+    //MARK: internal
+    
+    func requestItemFormat(
+        itemTreat:MVitaItemTreat)
+    {
+        createNewItem(itemTreat:itemTreat)
+        
+        changeStatus(
+            statusType:MVitaLinkStrategyRequestItemTreatFormat.self)
+        model?.linkCommand.requestItemFormat(
+            itemTreat:itemTreat)
+    }
+    
+    func requestFileName(
+        itemFormat:MVitaItemFormat)
+    {
+        guard
+            
+            let itemTreat:MVitaItemTreat = currentItem?.treat
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        currentItem?.format = itemFormat
+        
+        changeStatus(
+            statusType:MVitaLinkStrategyRequestItemTreatFileName.self)
+        model?.linkCommand.requestItemFileName(
+            itemTreat:itemTreat)
+    }
+    
+    func requestDateModified(
+        itemFileName:String)
+    {
+        guard
+            
+            let itemTreat:MVitaItemTreat = currentItem?.treat
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        currentItem?.name = itemFileName
+        
+        changeStatus(
+            statusType:MVitaLinkStrategyRequestItemTreatDateModified.self)
+        model?.linkCommand.requestItemDateModified(
+            itemTreat:itemTreat)
+    }
+    
+    func requestItemContent(
+        itemDateModified:Date)
+    {
+        currentItem?.dateModified = itemDateModified
+        
+        guard
+            
+            let itemFormat:MVitaItemFormat = currentItem?.format
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        requestItemContent(itemFormat:itemFormat)
+    }
+    
+    func requestItemData(
+        itemSize:UInt64)
+    {
+        guard
+            
+            let itemTreat:MVitaItemTreat = currentItem?.treat
+            
+        else
+        {
+            failed()
+            
+            return
+        }
+        
+        currentItem?.size = itemSize
+        
+        changeStatus(
+            statusType:MVitaLinkStrategyRequestItemTreatData.self)
+        model?.linkCommand.requestItemData(
+            itemTreat:itemTreat)
+    }
+    
+    func itemElementsReceived(
+        itemElements:[UInt32])
+    {
+        currentItem?.rawElements = itemElements
+        nextElement()
+    }
+    
+    func itemDataReceived(
+        itemData:Data)
+    {
+        currentItem?.data = itemData
+        nextElement()
+    }
+}
