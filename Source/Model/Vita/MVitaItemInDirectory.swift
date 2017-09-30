32fe3605e876c0c6f88b0d9cdfcc72f7687cab34
vauxhall <zero.griffin@gmail.com>
2017-09-30 16:41:11 -0500

    Add directory type to item

--- Source/Model/Vita/MVitaItemInDirectory.swift
+++ Source/Model/Vita/MVitaItemInDirectory.swift
@@ -3,12 +3,14 @@ import Foundation
 final class MVitaItemInDirectory:MVitaItemIn
 {
     let category:MVitaItemCategory
+    let directoryType:UInt32
     var rawElements:[UInt32]
     var elements:[MVitaItemInElement]
     
     init(itemTreat:MVitaItemTreat)
     {
         category = itemTreat.category
+        directoryType = itemTreat.directoryType
         rawElements = []
         elements = []
         
