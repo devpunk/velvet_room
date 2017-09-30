6306709dfcecfde31b1ccea63b531de31a7148fc
vauxhall <zero.griffin@gmail.com>
2017-09-30 03:16:18 -0500

    Look for item on request

--- Source/Model/Vita/MVitaLinkItem.swift
+++ Source/Model/Vita/MVitaLinkItem.swift
@@ -2,15 +2,58 @@ import Foundation
 
 extension MVitaLink
 {
+    private static let kLookItemPredicate:String = "name == \"%@\""
+    private static let kLookItemSorter:String = "dateReceived"
+    
+    //MARK: private
+    
+    private func itemFound(
+        item:DVitaItemDirectory,
+        event:MVitaPtpMessageInEvent)
+    {
+        
+    }
+    
+    private func itemNotFound(
+        event:MVitaPtpMessageInEvent)
+    {
+        sendResultSuccess(event:event)
+    }
+    
     //MARK: internal
     
     func requestItemStatus(
         itemStatus:MVitaItemStatus,
         event:MVitaPtpMessageInEvent)
     {
-        //TODO: validate with db
-        //          result success if not found
+        let predicate:NSPredicate = NSPredicate(
+            format:MVitaLink.kLookItemPredicate,
+            itemStatus.identifier)
+        let sorter:NSSortDescriptor = NSSortDescriptor(
+            key:MVitaLink.kLookItemSorter,
+            ascending:false)
+        let sorters:[NSSortDescriptor] = [
+            sorter]
         
-        sendResultSuccess(event:event)
+        database?.fetch(
+            predicate:predicate,
+            sorters:sorters)
+        { [weak self] (directories:[DVitaItemDirectory]) in
+            
+            guard
+            
+                let directory:DVitaItemDirectory = directories.first
+            
+            else
+            {
+                self?.itemNotFound(event:event)
+                
+                return
+            }
+            
+            self?.itemFound(
+                item:directory,
+                event:event)
+        }
     }
 }
