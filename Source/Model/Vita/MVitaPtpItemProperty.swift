7bdd48278a7956a703ab6f7b0dfb890ec6dd5ba4
vauxhall <zero.griffin@gmail.com>
2017-10-01 00:54:36 -0500

    Test getting more information

--- Source/Model/Vita/MVitaPtpItemProperty.swift
+++ Source/Model/Vita/MVitaPtpItemProperty.swift
@@ -4,6 +4,8 @@ enum MVitaPtpItemProperty:UInt32
 {
     case format                 = 56322
     case fileSize               = 56324
+    case test1                  = 56325 // association type
+    case test2                  = 56326 // association descr
     case fileName               = 56327
     case dateModified           = 56329
 }
