de0e9f4fb47b55b86c2996789e60670849bd1d16
vauxhall <zero.griffin@gmail.com>
2017-10-01 03:29:41 -0500

    Add storage path

--- Source/Model/Vita/MVitaLinkStorageRetrieve.swift
+++ Source/Model/Vita/MVitaLinkStorageRetrieve.swift
@@ -4,5 +4,24 @@ extension MVitaLink
 {
     //MARK: internal
     
-    
+    class func storagePath(
+        item:DVitaItemElement) -> URL?
+    {
+        guard
+        
+            let directoryName:String = item.directory?.localName,
+            let localName:String = item.localName
+        
+        else
+        {
+            return nil
+        }
+        
+        var path:URL = directoryPath(
+            directoryName:directoryName)
+        path.appendPathComponent(
+            localName)
+        
+        return path
+    }
 }
