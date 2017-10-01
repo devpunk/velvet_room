c4cdbdf3ab1a7cb8c1449bd8964191f1b7862e11
vauxhall <zero.griffin@gmail.com>
2017-10-01 14:12:40 -0500

    Factory key

--- Source/Model/Sfo/MSfoKeyFactory.swift
+++ Source/Model/Sfo/MSfoKeyFactory.swift
@@ -9,6 +9,21 @@ extension MSfo
         header:MSfoHeader,
         data:Data) -> MSfoKey?
     {
+        let byteStart:Int = item.keyOffset + header.keysOffset
+        let subdata:Data = data.subdata(start:byteStart)
         
+        guard
+        
+            let string:String = MSfoString.stringToFirstNull(
+                data:subdata)
+        
+        else
+        {
+            return nil
+        }
+        
+        let key:MSfoKey? = MSfoKey(rawValue:string)
+        
+        return key
     }
 }
--- Source/Model/Sfo/MSfoString.swift
+++ Source/Model/Sfo/MSfoString.swift
@@ -7,4 +7,37 @@ final class MSfoString
     private init() { }
     
     //MARK: internal
+    
+    class func stringToFirstNull(data:Data) -> String?
+    {
+        let array:[UInt8] = Array(data)
+        var validData:Data = Data()
+        
+        for item:UInt8 in array
+        {
+            guard
+            
+                item > 0
+            
+            else
+            {
+                break
+            }
+            
+            validData.append(item)
+        }
+        
+        guard
+            
+            let string:String = String(
+                data:validData,
+                encoding:String.Encoding.ascii)
+            
+        else
+        {
+            return nil
+        }
+        
+        return string
+    }
 }
