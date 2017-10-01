93410e6d5cc5fdb3bb2e96d695215a87f9f91c04
vauxhall <zero.griffin@gmail.com>
2017-10-01 17:59:24 -0500

    Add send items meta data

--- Source/Model/Vita/MVitaPtpEvent.swift
+++ Source/Model/Vita/MVitaPtpEvent.swift
@@ -2,6 +2,7 @@ enum MVitaPtpEvent:UInt16
 {
     case unknown
     case sendItemsCount                 = 49412
+    case sendItemsMetaData              = 49413
     case requestItemStatus              = 49423
     case requestSettings                = 49426
     case sendStorageSize                = 49433
