6c0e7cbdcbc429e417134fe69f0fa94e80f0685b
vauxhall <zero.griffin@gmail.com>
2017-10-01 00:56:41 -0500

    Create testing strategy

---
+++ Source/Model/Vita/MVitaLinkStrategyRequestItemTreatTest.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+final class MVitaLinkStrategyRequestItemTreatTest:MVitaLinkStrategyRequestItemTreatProtocol
+{
+    //MARK: internal
+    
+    func success(
+        strategy:MVitaLinkStrategyRequestItemTreat)
+    {
+        
+    }
+}
