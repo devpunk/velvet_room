3f8555488ef9cc6daff74107609e4173bfb1b485
vauxhall <zero.griffin@gmail.com>
2017-10-01 06:35:27 -0500

    Use struct instead of class

--- Source/Model/Sfo/MSfo.swift
+++ Source/Model/Sfo/MSfo.swift
@@ -1,6 +1,13 @@
 import Foundation
 
-final class MSfo
+struct MSfo
 {
-    
+    let category:String
+    let parentalLevel:Int
+    let title:String?
+    let saveDataDetail:String?
+    let saveDataDirectory:String?
+    let saveDataFileList:String?
+    let saveDataParams:String?
+    let saveDataTitle:String?
 }
