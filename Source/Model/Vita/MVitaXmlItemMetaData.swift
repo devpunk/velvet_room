4b577f07dd9068893414e47680f07d4a43d64882
vauxhall <zero.griffin@gmail.com>
2017-10-01 19:10:40 -0500

    Create factory item metadata

---
+++ Source/Model/Vita/MVitaXmlItemMetaData.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+final class MVitaXmlItemMetaData
+{
+    //MARK: private
+    
+    private init() { }
+    
+    //MARK: internal
+    
+    class func factoryMetaData(
+        items:[DVitaItem]) -> Data
+    {
+        
+    }
+}
