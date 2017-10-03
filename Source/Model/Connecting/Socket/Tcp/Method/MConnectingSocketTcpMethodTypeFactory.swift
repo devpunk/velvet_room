44728ce3574b7459e2c9304383fe2c1dc2dc0b8b
vauxhall <zero.griffin@gmail.com>
2017-10-03 04:42:12 -0500

    Replace switch with method map

--- Source/Model/Connecting/Socket/Tcp/Method/MConnectingSocketTcpMethodTypeFactory.swift
+++ Source/Model/Connecting/Socket/Tcp/Method/MConnectingSocketTcpMethodTypeFactory.swift
@@ -2,36 +2,21 @@ import Foundation
 
 extension MConnectingSocketTcpMethodType
 {
-    private static func factoryMethod(
-        type:MConnectingSocketTcpMethodType) -> MConnectingSocketTcpMethodProtocol.Type
-    {
-        switch type
-        {
-        case MConnectingSocketTcpMethodType.showpin:
-            
-            return MConnectingSocketTcpMethodShowpin.self
-            
-        case MConnectingSocketTcpMethodType.register:
-            
-            return MConnectingSocketTcpMethodRegister.self
-            
-        case MConnectingSocketTcpMethodType.registerResult:
-            
-            return MConnectingSocketTcpMethodRegisterResult.self
-            
-        case MConnectingSocketTcpMethodType.registerCancel:
-            
-            return MConnectingSocketTcpMethodRegisterCancel.self
-            
-        case MConnectingSocketTcpMethodType.connect:
-            
-            return MConnectingSocketTcpMethodConnect.self
-            
-        case MConnectingSocketTcpMethodType.standby:
-            
-            return MConnectingSocketTcpMethodStandby.self
-        }
-    }
+    private static let kMethodMap:[
+        MConnectingSocketTcpMethodType:
+        MConnectingSocketTcpMethodProtocol.Type] = [
+            MConnectingSocketTcpMethodType.showpin:
+                MConnectingSocketTcpMethodShowpin.self,
+            MConnectingSocketTcpMethodType.register:
+                MConnectingSocketTcpMethodRegister.self,
+            MConnectingSocketTcpMethodType.registerResult:
+                MConnectingSocketTcpMethodRegisterResult.self,
+            MConnectingSocketTcpMethodType.registerCancel:
+                MConnectingSocketTcpMethodRegisterCancel.self,
+            MConnectingSocketTcpMethodType.connect:
+                MConnectingSocketTcpMethodConnect.self,
+            MConnectingSocketTcpMethodType.standby:
+                MConnectingSocketTcpMethodStandby.self]
     
     //MARK: internal
     
@@ -42,15 +27,15 @@ extension MConnectingSocketTcpMethodType
         guard
         
             let type:MConnectingSocketTcpMethodType = MConnectingSocketTcpMethodType(
-                rawValue:name)
+                rawValue:name),
+            let methodType:MConnectingSocketTcpMethodProtocol.Type = kMethodMap[
+                type]
         
         else
         {
             return nil
         }
         
-        let methodType:MConnectingSocketTcpMethodProtocol.Type = factoryMethod(
-            type:type)
         let method:MConnectingSocketTcpMethodProtocol = methodType.init(
             received:received)
         
