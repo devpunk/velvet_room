3c17aff19dd20b06a7f9889f29210a3f320db319
vauxhall <zero.griffin@gmail.com>
2017-10-02 19:24:16 -0500

    Create keys struct

---
+++ Source/Model/Vita/MVitaXml.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct MVitaXml
+{
+    static let kKeyObjectMetaData:String = "objectMetadata"
+}
---
+++ Source/Model/Vita/MVitaXmlThumbnailMetaData.swift
@@ -0,0 +1,14 @@
+import Foundation
+import XmlHero
+
+final class MVitaXmlThumbnailMetaData
+{
+    private static let kKeyRoot:String = "objectMetadata"
+    private static let kKeyFolder:String = "folder"
+    private static let kKeyFile:String = "file"
+    private static let kKeyIndex:String = "index"
+    
+    //MARK: private
+    
+    private init() { }
+}
