52a9675971ef3263f469a06d6605d8614666be91
vauxhall <zero.griffin@gmail.com>
2017-10-03 15:28:59 -0500

    Remove print

--- Source/Model/Vita/MVitaLinkStrategySendData.swift
+++ Source/Model/Vita/MVitaLinkStrategySendData.swift
@@ -48,8 +48,6 @@ class MVitaLinkStrategySendData:MVitaLinkStrategyProtocol
     private func changeStatus(
         statusType:MVitaLinkStrategySendDataStatusProtocol.Type)
     {
-        print(statusType)
-        
         DispatchQueue.global(
             qos:DispatchQoS.QoSClass.background).async
         { [weak self] in
