e5d9c6f775a0e4eb557b070e5b57508645af334c
vauxhall <zero.griffin@gmail.com>
2017-10-03 17:01:24 -0500

    Create strategy send item

---
+++ Source/Model/Vita/MVitaLinkStrategySendItem.swift
@@ -0,0 +1,154 @@
+import Foundation
+
+final class MVitaLinkStrategySendItem:
+    MVitaLinkStrategySendData,
+    MVitaLinkStrategyDatabaseProtocol,
+    MVitaLinkStrategyEventProtocol
+{
+    private weak var database:Database?
+    private var event:MVitaPtpMessageInEvent?
+    
+    override func failed()
+    {
+        let message:String = String.localizedModel(
+            key:"MVitaLinkStrategySendItem_messageFailed")
+        model?.errorCloseConnection(
+            message:message)
+    }
+    
+    override func success()
+    {
+        guard
+            
+            let event:MVitaPtpMessageInEvent = self.event
+            
+            else
+        {
+            failed()
+            
+            return
+        }
+        
+        model?.sendResultSuccess(event:event)
+    }
+    
+    //MARK: database protocol
+    
+    func config(database:Database)
+    {
+        self.database = database
+    }
+    
+    //MARK: event protocol
+    
+    func config(event:MVitaPtpMessageInEvent)
+    {
+        self.event = event
+        let totalParameters:Int = event.parameters.count
+        
+        guard
+            
+            totalParameters > kItemParameterIndex,
+            let database:Database = self.database
+            
+            else
+        {
+            failed()
+            
+            return
+        }
+        
+        let unsignedItemIndex:UInt32 = event.parameters[kItemParameterIndex] - 1
+        let itemIndex:Int = Int(unsignedItemIndex)
+        let sorters:[NSSortDescriptor] = MVitaLink.factorySortersForIdentifier()
+        
+        sendThumbnail(
+            itemIndex:itemIndex,
+            sorters:sorters,
+            database:database,
+            event:event)
+    }
+    
+    //MARK: private
+    
+    private func sendThumbnail(
+        itemIndex:Int,
+        sorters:[NSSortDescriptor],
+        database:Database,
+        event:MVitaPtpMessageInEvent)
+    {
+        database.fetch(
+            limit:itemIndex,
+            sorters:sorters)
+        { [weak self] (identifiers:[DVitaIdentifier]) in
+            
+            let totalIdentifiers:Int = identifiers.count
+            
+            guard
+                
+                totalIdentifiers > itemIndex
+                
+                else
+            {
+                self?.failed()
+                
+                return
+            }
+            
+            let identifier:DVitaIdentifier = identifiers[itemIndex]
+            
+            guard
+                
+                let directory:DVitaItemDirectory = identifier.items?.lastObject as? DVitaItemDirectory
+                
+                else
+            {
+                self?.failed()
+                
+                return
+            }
+            
+            self?.sendThumbnail(
+                directory:directory,
+                event:event)
+        }
+    }
+    
+    private func sendThumbnail(
+        directory:DVitaItemDirectory,
+        event:MVitaPtpMessageInEvent)
+    {
+        MVitaXmlThumbnail.factoryMetadata(directory:directory)
+        { (data:Data?) in
+            
+            guard
+                
+                let data:Data = data
+                
+                else
+            {
+                self.failed()
+                
+                return
+            }
+            
+            self.sendData(
+                data:data,
+                event:event)
+        }
+    }
+    
+    private func sendData(
+        data:Data,
+        event:MVitaPtpMessageInEvent)
+    {
+        let message:MVitaPtpMessageOutEventCommand = MVitaPtpMessageOutEventCommand(
+            event:event,
+            dataPhase:MVitaPtpDataPhase.send,
+            command:MVitaPtpCommand.sendItemThumbnail)
+        
+        send(
+            data:data,
+            message:message)
+    }
+}
