edce06c6f1968080834eaab0225e4533d6c45e25
vauxhall <zero.griffin@gmail.com>
2017-10-08 13:16:43 +0200

    Load save data

--- Source/Model/Home/MHome.swift
+++ Source/Model/Home/MHome.swift
@@ -4,9 +4,12 @@ final class MHome:Model<ArchHome>
 {
     var database:Database?
     var account:DAccount?
+    var saveDataItems:[MHomeSaveDataItem]
     
     required init()
     {
+        saveDataItems = []
+        
         super.init()
     }
 }
--- Source/Model/Home/MHomeSaveData.swift
+++ Source/Model/Home/MHomeSaveData.swift
@@ -2,10 +2,32 @@ import Foundation
 
 extension MHome
 {
+    //MARK: private
+    
+    private func loadSaveData(
+        database:Database)
+    {
+        
+    }
+    
     //MARK: internal
     
     func loadSaveData()
     {
+        guard
         
+            let database:Database = self.database
+        
+        else
+        {
+            return
+        }
+        
+        DispatchQueue.global(
+            qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.loadSaveData(database:database)
+        }
     }
 }
