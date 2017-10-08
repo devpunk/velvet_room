c8c2fc076ee377aba4ca4ec4ef952330e0ed0a96
vauxhall <zero.griffin@gmail.com>
2017-10-08 13:29:11 +0200

    Create factory extension

--- Source/Model/Home/MHomeSaveData.swift
+++ Source/Model/Home/MHomeSaveData.swift
@@ -7,7 +7,24 @@ extension MHome
     private func loadSaveData(
         database:Database)
     {
+        database.fetch
+        { [weak self] (identifiers:[DVitaIdentifier]) in
+            
+            self?.identifiersFetched(identifiers:identifiers)
+        }
+    }
+    
+    private func identifiersFetched(
+        identifiers:[DVitaIdentifier])
+    {
+        var items:[MHomeSaveDataItem] = []
         
+        for identifier:DVitaIdentifier in identifiers
+        {
+            
+        }
+        
+        self.saveDataItems = items
     }
     
     //MARK: internal
---
+++ Source/Model/Home/MHomeSaveDataItemFactory.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+extension MHomeSaveDataItem
+{
+    //MARK: internal
+}
