2f3a6af1e85d039c2a7832df4e9bf7dc920986cb
vauxhall <zero.griffin@gmail.com>
2017-10-09 07:25:53 +0200

    Factory last update

--- Source/Model/Home/MHomeSaveDataItemFactory.swift
+++ Source/Model/Home/MHomeSaveDataItemFactory.swift
@@ -21,19 +21,59 @@ extension MHomeSaveDataItem
         return gameName
     }
     
+    private class func factoryLastUpdate(
+        identifier:DVitaIdentifier,
+        dateFormatter:DateFormatter) -> String?
+    {
+        guard
+            
+            let directories:[DVitaItemDirectory] = identifier.items?.array as? [
+                DVitaItemDirectory],
+            let lastUpdate:TimeInterval = directories.last?.dateModified
+            
+        else
+        {
+            return nil
+        }
+        
+        let date:Date = Date(timeIntervalSince1970:lastUpdate)
+        let dateString:String = dateFormatter.string(from:date)
+        
+        return dateString
+    }
+    
     //MARK: internal
     
+    class func factoryDateFormatter() -> DateFormatter
+    {
+        let dateFormatter:DateFormatter = DateFormatter()
+        dateFormatter.timeStyle = DateFormatter.Style.medium
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        
+        return dateFormatter
+    }
+    
     class func factoryItem(
-        identifier:DVitaIdentifier) -> MHomeSaveDataItem?
+        identifier:DVitaIdentifier,
+        dateFormatter:DateFormatter) -> MHomeSaveDataItem?
     {
         guard
         
             let gameName:String = factoryGameName(
-                identifier:identifier)
+                identifier:identifier),
+            let lastUpdate:String = factoryLastUpdate(
+                identifier:identifier,
+                dateFormatter:dateFormatter)
         
         else
         {
             return nil
         }
+        
+        let item:MHomeSaveDataItem = MHomeSaveDataItem(
+            coredataModel:identifier,
+            gameName:gameName,
+            lastUpdated:lastUpdate,
+            thumbnail:)
     }
 }
