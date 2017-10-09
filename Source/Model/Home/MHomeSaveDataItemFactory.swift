20af4a0461eb449f6d1368b21ba512ff7703c0aa
vauxhall <zero.griffin@gmail.com>
2017-10-09 07:17:49 +0200

    Factory gamename

--- Source/Model/Home/MHomeSaveDataItemFactory.swift
+++ Source/Model/Home/MHomeSaveDataItemFactory.swift
@@ -2,5 +2,38 @@ import Foundation
 
 extension MHomeSaveDataItem
 {
+    //MARK: private
+    
+    private class func factoryGameName(
+        identifier:DVitaIdentifier) -> String?
+    {
+        guard
+        
+            let directories:[DVitaItemDirectory] = identifier.items?.array as? [
+                DVitaItemDirectory],
+            let gameName:String = directories.last?.sfoTitle
+        
+        else
+        {
+            return nil
+        }
+        
+        return gameName
+    }
+    
     //MARK: internal
+    
+    class func factoryItem(
+        identifier:DVitaIdentifier) -> MHomeSaveDataItem?
+    {
+        guard
+        
+            let gameName:String = factoryGameName(
+                identifier:identifier)
+        
+        else
+        {
+            return nil
+        }
+    }
 }
