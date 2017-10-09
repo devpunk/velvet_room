13afbf41bc089d0ce30ce5179e3fa80965cc7d42
vauxhall <zero.griffin@gmail.com>
2017-10-09 06:58:26 +0200

    Init save item

--- Source/Model/Home/MHomeSaveDataItem.swift
+++ Source/Model/Home/MHomeSaveDataItem.swift
@@ -1,6 +1,21 @@
-import Foundation
+import UIKit
 
 final class MHomeSaveDataItem
 {
+    weak var coredataModel:DVitaIdentifier?
+    let gameName:String
+    let lastUpdated:String
+    let thumbnail:UIImage?
     
+    init(
+        coredataModel:DVitaIdentifier,
+        gameName:String,
+        lastUpdated:String,
+        thumbnail:UIImage?)
+    {
+        self.coredataModel = coredataModel
+        self.gameName = gameName
+        self.lastUpdated = lastUpdated
+        self.thumbnail = thumbnail
+    }
 }
