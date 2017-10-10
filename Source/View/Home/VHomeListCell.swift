120ddc3a6633ea853ec26d7933d71691624848b8
vauxhall <zero.griffin@gmail.com>
2017-10-10 05:13:51 +0200

    Add image view to cell

--- Source/View/Home/VHomeListCell.swift
+++ Source/View/Home/VHomeListCell.swift
@@ -2,16 +2,44 @@ import UIKit
 
 final class VHomeListCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    private weak var layoutImageWidth:NSLayoutConstraint!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:imageView,
+            toView:self)
+        layoutImageWidth = NSLayoutConstraint.width(
+            view:imageView)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    override func layoutSubviews()
+    {
+        let height:CGFloat = bounds.height
+        layoutImageWidth.constant = height
+        
+        super.layoutSubviews()
     }
     
     override var isSelected:Bool
