d927a553f206b68f41ba47485c0a3a15b6bdfd05
vauxhall <zero.griffin@gmail.com>
2017-10-11 18:38:47 -0500

    Layout view bar

--- Source/View/SaveData/VSaveData.swift
+++ Source/View/SaveData/VSaveData.swift
@@ -2,11 +2,60 @@ import UIKit
 
 final class VSaveData:ViewMain
 {
+    private weak var layoutBarHeight:NSLayoutConstraint!
+    
     override var panBack:Bool
     {
         get
         {
             return true
         }
+    }
+    
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CSaveData = controller as? CSaveData
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.width
+        layoutBarHeight.constant = width
+        
+        super.layoutSubviews()
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CSaveData)
+    {
+        let viewBar:VSaveDataBar = VSaveDataBar(controller:controller)
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        layoutBarHeight = NSLayoutConstraint.height(
+            view:viewBar)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
     }
 }
