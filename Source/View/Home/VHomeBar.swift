5f710d0149d569286df6b6d131062a6221342c69
vauxhall <zero.griffin@gmail.com>
2017-10-11 17:02:13 -0500

    Add border to bar

--- Source/View/Home/VHomeBar.swift
+++ Source/View/Home/VHomeBar.swift
@@ -2,9 +2,27 @@ import UIKit
 
 final class VHomeBar:View<ArchHome>
 {
+    private let kBorderHeight:CGFloat = 1
+    
     required init(controller:CHome)
     {
         super.init(controller:controller)
+        backgroundColor = UIColor.white
+        
+        let border:VBorder = VBorder(
+            colour:UIColor.colourBackgroundDark)
+        
+        addSubview(border)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:border,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:border,
+            constant:kBorderHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:border,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
