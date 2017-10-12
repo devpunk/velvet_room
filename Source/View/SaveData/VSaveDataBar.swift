e948f3be19cce83c918e82b8a7ae5240971d75b3
vauxhall <zero.griffin@gmail.com>
2017-10-12 11:55:47 -0500

    Add background to bar

--- Source/View/SaveData/VSaveDataBar.swift
+++ Source/View/SaveData/VSaveDataBar.swift
@@ -2,5 +2,29 @@ import UIKit
 
 final class VSaveDataBar:View<ArchSaveData>
 {
+    required init(controller:CSaveData)
+    {
+        super.init(controller:controller)
+        
+        factoryViews()
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews()
+    {
+        let viewBackground:VSaveDataBarBackground = VSaveDataBarBackground(
+            controller:controller)
+        
+        addSubview(viewBackground)
+        
+        NSLayoutConstraint.equals(
+            view:viewBackground,
+            toView:self)
+    }
 }
--- Source/View/SaveData/VSaveDataBarBackground.swift
+++ Source/View/SaveData/VSaveDataBarBackground.swift
@@ -2,5 +2,34 @@ import UIKit
 
 final class VSaveDataBarBackground:View<ArchSaveData>
 {
+    required init(controller:CSaveData)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        
+        factoryViews()
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews()
+    {
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = controller.model.item?.thumbnail
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
 }
