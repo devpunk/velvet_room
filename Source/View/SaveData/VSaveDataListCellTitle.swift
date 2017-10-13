cf3df4ab4f44409f89fbbde2e67f7d8cca4545b4
vauxhall <zero.griffin@gmail.com>
2017-10-13 09:39:42 -0500

    Create cell title

---
+++ Source/View/SaveData/VSaveDataListCellTitle.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class VSaveDataListCellTitle:VSaveDataListCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
