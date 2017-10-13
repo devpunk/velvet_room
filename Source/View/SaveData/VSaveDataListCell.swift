4566e1dae91e425da2ad415e54436504b5fb00d8
vauxhall <zero.griffin@gmail.com>
2017-10-13 11:16:25 -0500

    Add border to cell

--- Source/View/SaveData/VSaveDataListCell.swift
+++ Source/View/SaveData/VSaveDataListCell.swift
@@ -3,12 +3,30 @@ import UIKit
 class VSaveDataListCell:UICollectionViewCell
 {
     private(set) weak var controller:CSaveData?
+    private let kBorderMargin:CGFloat = 10
+    private let kBorderHeight:CGFloat = 1
     
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.white
+        
+        let border:VBorder = VBorder(
+            colour:UIColor.colourBackgroundGray)
+        
+        addSubview(border)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:border,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:border,
+            constant:kBorderHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:border,
+            toView:self,
+            margin:kBorderMargin)
     }
     
     required init?(coder:NSCoder)
