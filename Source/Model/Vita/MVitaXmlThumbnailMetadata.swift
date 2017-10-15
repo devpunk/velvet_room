70300b7125cb23021e980e8cc42351b80029ed2a
vauxhall <zero.griffin@gmail.com>
2017-10-15 18:34:25 -0500

    Update thumbnail metadata

--- Source/Model/Vita/MVitaXmlThumbnailMetadata.swift
+++ Source/Model/Vita/MVitaXmlThumbnailMetadata.swift
@@ -3,21 +3,11 @@ import XmlHero
 
 extension MVitaXmlThumbnail
 {
-    private static let kAspectRatio:String = "1,000000"
-    private static let kCodecType:Int = 18
-    private static let kWidth:Int = 144
-    private static let kHeight:Int = 80
-    private static let kType:Int = 0
-    private static let kOrientationType:Int = 1
-    private static let kFromType:Int = 2
-    
     //MARK: private
     
-    private class func factoryMetadataModel(
-        thumbnailData:Data) -> [String:Any]
+    private class func factoryMetadataModel() -> [String:Any]
     {
-        let modelContent:[String:Any] = factoryContentModel(
-            thumbnailData:thumbnailData)
+        let modelContent:[String:Any] = factoryContentModel()
         let modelThumbnail:[String:Any] = [
             MVitaXml.kKeyThumbnail:modelContent]
         let model:[String:Any] = [
@@ -26,8 +16,7 @@ extension MVitaXmlThumbnail
         return model
     }
     
-    private class func factoryContentModel(
-        thumbnailData:Data) -> [String:Any]
+    private class func factoryContentModel() -> [String:Any]
     {
         let model:[String:Any] = [:]
         
@@ -36,12 +25,9 @@ extension MVitaXmlThumbnail
     
     //MARK: internal
     
-    class func factoryXml(
-        thumbnailData:Data,
-        completion:@escaping((Data?) -> ()))
+    class func factoryXml(completion:@escaping((Data?) -> ()))
     {
-        let model:[String:Any] = factoryMetadataModel(
-            thumbnailData:thumbnailData)
+        let model:[String:Any] = factoryMetadataModel()
         
         Xml.data(object:model)
         { (data:Data?, error:XmlError?) in
