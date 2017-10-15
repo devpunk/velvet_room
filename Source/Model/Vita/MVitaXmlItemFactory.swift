d9d3c984f9812be5c77b27b25eb2253d4c5803ac
vauxhall <zero.griffin@gmail.com>
2017-10-15 18:34:54 -0500

    Create metadata extension

--- Source/Model/Vita/MVitaXmlItemFactory.swift
+++ Source/Model/Vita/MVitaXmlItemFactory.swift
@@ -4,4 +4,23 @@ extension MVitaXmlItem
 {
     //MARK: internal
     
+    class func factoyItem(
+        directory:DVitaIdentifier) -> Data?
+    {
+        guard
+            
+            let pngs:[DVitaItemElementPng] = directory.png?.array as? [
+                DVitaItemElementPng],
+            let thumbnail:DVitaItemElement = findSmallerImage(
+                pngs:pngs),
+            let data:Data = MVitaLink.elementData(
+                element:thumbnail)
+            
+            else
+        {
+            return nil
+        }
+        
+        return data
+    }
 }
